8273f8f11eb329ec274030df118c9b9fca8914fd
Shawn Taylor <shawn@cypress.io>
2022-04-20 17:14:52 -0400

    Use Storybook Composition to combine storybooks

--- storybook-react/.storybook/main.js
+++ storybook-react/.storybook/main.js
@@ -12,6 +12,34 @@ module.exports = {
     // "@storybook/addon-interactions",
   ],
   framework: '@storybook/react',
+  refs: {
+    react: {
+      title: "Vue",
+      url: process.env.VERCEL_URL
+        ? process.env.VERCEL_URL + "/storybook-vue/storybook-static"
+        : "http://localhost:6007",
+      expanded: true,
+    },
+  },
+  framework: "@storybook/react",
+  managerWebpack: (config) => {
+    config.module.rules = [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        use: {
+          // without this, storybook composition only works if the external storybook(s) are launched before the primary one
+          // https://github.com/storybookjs/storybook/issues/13650#issuecomment-773375007
+          loader: "string-replace-loader",
+          options: {
+            search: /"type": "unknown"/g,
+            replace: () => '"type": "server-checked"',
+          },
+        },
+      },
+      ...config.module.rules,
+    ]
+    return config
+  },
   webpackFinal: (config) => {
     config.plugins.push(
       CyCSSWebpackPlugin({
