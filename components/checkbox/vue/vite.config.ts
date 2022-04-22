fad437efa67f35e029516cabfd7fd514aabbeca3
Bart Ledoux <ledouxb@gmail.com>
2022-04-22 16:03:21 -0500

    better vue typings

--- components/checkbox/vue/vite.config.ts
+++ components/checkbox/vue/vite.config.ts
@@ -2,6 +2,6 @@ import * as path from 'path';
 import generateViteConfig from '../../root.vite.config';
 
 export default generateViteConfig({
-  entry: path.resolve(__dirname, './Checkbox.vue'),
+  entry: path.resolve(__dirname, './index.ts'),
   name: 'Checkbox',
 });
