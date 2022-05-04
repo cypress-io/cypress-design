a03d6c4f81ae93f2113f081b80c7b8f4fda7dab3
Bart Ledoux <bart@cypress.io>
2022-05-04 13:13:50 -0500

    add windi and icon to vue test app

--- test/vue-app/src/App.vue
+++ test/vue-app/src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import Checkbox from "@cypress-design/vue-checkbox"
+import Icon from "@cypress-design/vue-icon"
 
 const checked = ref(false)
 </script>
 
 <template>
   <div style="width:500px; text-align:center; margin: auto;">
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
     <Checkbox v-model="checked" label="Checkbox" id="Sample" />
     <p v-if="checked">Checked</p>
+    <Icon iconId="book" darkColor="amber-500" size="24" />
   </div>
 </template>
--- test/vue-app/vite.config.ts
+++ test/vue-app/vite.config.ts
@@ -1,14 +1,22 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'url';
+import { CyCSSVitePlugin } from '@cypress-design/css';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  plugins: [
+    vue(),
+    CyCSSVitePlugin({
+      scan: {
+        include: ['/src/**/*.@(vue|tsx|ts)'],
+      },
+    }),
+  ],
+});
