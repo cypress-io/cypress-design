ef273a2bcf6f436e7f3001478d54c3f092b3e603
Shawn Taylor <shawn@cypress.io>
2022-05-04 15:02:59 -0400

    [DESIGN-940] Component testing (#9)
    * Install Cypress; add basic tests
    
    * Get Vue tests working
    
    * Clean up
    
    * Add yarn commands and cypress github action
    
    * Add hygen for Cypress tests
    
    * Try again on github action
    
    * fix cypress component testing
    
    * update github action
    
    * use yarn cache to accelerate CI
    
    Co-authored-by: Bart Ledoux <bart@cypress.io>

---
+++ components/checkbox/react/Checkbox.cy.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { mount } from 'cypress/react';
+import { Checkbox } from './Checkbox';
+
+describe('Checkbox', () => {
+  it('renders', () => {
+    let isChecked = true;
+
+    mount(
+      <Checkbox
+        label="Welcome guide settings"
+        id="welcome-opt-out"
+        modelValue={isChecked}
+        onChange={() => (isChecked = !isChecked)}
+      />
+    );
+  });
+});
