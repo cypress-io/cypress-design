b17f063169388de513b050168ec17fe3529e86ac
Bart Ledoux <bart@cypress.io>
2022-05-05 10:34:46 -0500

    add new context example icons

--- test/vue-app/src/App.vue
+++ test/vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import Checkbox from "@cypress-design/vue-checkbox"
-import Icon from "@cypress-design/vue-icon"
+import Icon, { IconBookCode } from "@cypress-design/vue-icon"
 
 const checked = ref(false)
 </script>
@@ -10,6 +10,7 @@ const checked = ref(false)
   <div style="width:500px; text-align:center; margin: auto;">
     <Checkbox v-model="checked" label="Checkbox" id="Sample" />
     <p v-if="checked">Checked</p>
+    <IconBookCode size="48" light-color="amber-300" dark-color="amber-500" />
     <Icon iconId="book" darkColor="amber-500" size="24" />
   </div>
 </template>
