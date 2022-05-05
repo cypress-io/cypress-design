56585c825b82fab4db3f50725837768a2578293e
Bart Ledoux <bart@cypress.io>
2022-05-05 10:35:33 -0500

    make sure icon registry can be used in node

--- components/icon/vue/Icon.ts
+++ components/icon/vue/Icon.ts
@@ -2,18 +2,19 @@ import { type Icon, compileIcon } from '@cypress-design/icon-registry';
 import { h, type SVGAttributes } from 'vue';
 
 export default (props: Icon & SVGAttributes) => {
-  const {
-    // all these constants are going to be dumped
-    iconId,
-    size: _s,
-    darkColor,
-    lightColor,
-    secondaryDarkColor,
-    secondaryLightColor,
-    class: className,
-    ...attributes // we want to extract all standard SVG attributes separately
-  } = props as any;
-  const { size, compiledClasses, body } = compileIcon(props);
+  return h('svg', compileVueIconProperties(compileIcon(props)));
+};
+
+export const compileVueIconProperties = ({
+  body,
+  compiledClasses,
+  size,
+  ...attributes
+}: SVGAttributes & {
+  body: string;
+  compiledClasses: string[];
+  size: string;
+}) => {
   const componentProps: any = {
     width: size,
     height: size,
@@ -21,11 +22,11 @@ export default (props: Icon & SVGAttributes) => {
     innerHTML: body,
     ...attributes, // add all standard attributes back to the svg tag
   };
-  if (className) {
-    compiledClasses.push(className);
+  if (attributes.class) {
+    compiledClasses.push(attributes.class);
   }
   if (compiledClasses.length) {
     componentProps.class = compiledClasses.join(' ');
   }
-  return h('svg', componentProps);
+  return componentProps;
 };
--- icon-registry/src/compileIcon.ts
+++ icon-registry/src/compileIcon.ts
@@ -2,24 +2,16 @@ import { type Icon, icons } from './icons';
 import { iconSet } from './iconsList';
 import camelCase from 'camelcase';
 
-export const compileIcon = ({
-  iconId,
-  size,
-  darkColor,
-  lightColor,
-  secondaryDarkColor,
-  secondaryLightColor,
-}: Icon) => {
+export const compileIcon = (props: Icon) => {
+  const { iconId } = props;
   const { availableSizes } = icons[iconId];
-  const sizeWithDefault =
-    size ?? (availableSizes.length >= 1 ? availableSizes[0] : '');
+
+  const { sizeWithDefault, compiledClasses } = getComponentAttributes({
+    ...(props as any),
+    availableSizes,
+  });
+
   const iconIdWithSize = camelCase(`${iconId}_x${sizeWithDefault}`);
-  const compiledClasses = [
-    darkColor && `icon-dark-${darkColor}`,
-    lightColor && `icon-light-${lightColor}`,
-    secondaryDarkColor && `icon-dark-secondary-${secondaryDarkColor}`,
-    secondaryLightColor && `icon-light-secondary-${secondaryLightColor}`,
-  ].filter((a) => a);
   const iconData = iconSet.find((i) => i.name === iconIdWithSize);
   if (!iconData) {
     throw new Error(`icon '${iconId}' at size ${sizeWithDefault} not found`);
@@ -29,4 +21,32 @@ export const compileIcon = ({
     compiledClasses,
     body: iconData.data,
   };
+};
+
+export const getComponentAttributes = ({
+  size,
+  availableSizes,
+  darkColor,
+  lightColor,
+  secondaryDarkColor,
+  secondaryLightColor,
+}: {
+  size: string;
+  availableSizes: readonly string[];
+  darkColor?: string;
+  lightColor?: string;
+  secondaryDarkColor?: string;
+  secondaryLightColor?: string;
+}) => {
+  const sizeWithDefault =
+    size ?? (availableSizes.length >= 1 ? availableSizes[0] : '');
+
+  const compiledClasses = [
+    darkColor && `icon-dark-${darkColor}`,
+    lightColor && `icon-light-${lightColor}`,
+    secondaryDarkColor && `icon-dark-secondary-${secondaryDarkColor}`,
+    secondaryLightColor && `icon-light-secondary-${secondaryLightColor}`,
+  ].filter((a) => a);
+
+  return { compiledClasses, sizeWithDefault };
 };
