48a152c183a6762a5f4727d919ae3791b652e058
Bart Ledoux <bart@cypress.io>
2022-05-05 21:00:28 -0500

    install windicss to cypress CT

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -19,18 +19,4 @@ import './commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-import { mount } from 'cypress/vue';
-
-// Augment the Cypress namespace to include type definitions for
-// your custom command.
-// Alternatively, can be defined in cypress/support/component.d.ts
-// with a <reference path="./component" /> at the top of your spec.
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount;
-    }
-  }
-}
-
-Cypress.Commands.add('mount', mount);
+import 'virtual:windi.css';
