5a50c5fa6b36175ba338d5e25210403f34494e03
Bart Ledoux <bart@cypress.io>
2022-05-09 12:08:39 -0500

    fix: make icon arguments clearer in the docs

--- components/icon/vue/Icon.stories.tsx
+++ components/icon/vue/Icon.stories.tsx
@@ -1,25 +1,34 @@
+import { iconsMetadata } from '@cypress-design/icon-registry';
 import Icon from './Icon'
+
+const colors = ['jade', 'teal', 'red', 'indigo', 'blue', 'purple', 'gray'];
 
 export default {
   title: 'Icon',
   component: Icon,
   argTypes: {
     name: {
+      options: Object.keys(iconsMetadata),
       control: { type: 'select' },
     },
     size: {
+      options: ['8', '16', '32', '48', '120'],
       control: { type: 'select' },
     },
     strokeColor: {
+      options: colors.map(color => `${color}-500`),
       control: { type: 'select' },
     },
     fillColor: {
+      options: colors.map(color => `${color}-100`),
       control: { type: 'select' },
     },
     secondaryStrokeColor: {
+      options: colors.map(color => `${color}-500`),
       control: { type: 'select' },
     },
     secondaryFillColor: {
+      options: colors.map(color => `${color}-100`),
       control: { type: 'select' },
     }
   }
