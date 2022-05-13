360b80c1db76a79e2a5824751999950e4ea45620
Bart Ledoux <bart@cypress.io>
2022-05-13 13:10:32 -0500

    add icon webpack test

--- test/webpack-react-app/src/App.tsx
+++ test/webpack-react-app/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { FunctionComponent, useState } from 'react';
 import { hot } from 'react-hot-loader/root';
 import Checkbox from '@cypress-design/react-checkbox';
+import Icon from '@cypress-design/react-icon';
 
 interface Props {
   name: string;
@@ -18,6 +19,7 @@ const App: FunctionComponent<Props> = ({ name }) => {
         onChange={() => setChecked(!isChecked)}
         modelValue={isChecked}
       />
+      <Icon name="book-code" fillColor="red-300" strokeColor="green-600"/>
     </>
   );
 };
