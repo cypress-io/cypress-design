05516c5890b9549d83d89c942e9652efc32f2f49
Bart Ledoux <bart@cypress.io>
2022-05-17 11:54:27 -0500

    remove sourcemap issues

--- components/root.rollup.config.js
+++ components/root.rollup.config.js
@@ -17,15 +17,6 @@ export default ({ input, plugins = [] }) => ({
       sourcemap: true,
     },
   ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.build.json',
-      declaration: false,
-      declarationMap: false,
-    }),
-    ...plugins,
-  ],
+  plugins: [resolve(), commonjs(), typescript(), ...plugins],
   external: ['clsx', 'react', '@cypress-design/icon-registry'],
 });
