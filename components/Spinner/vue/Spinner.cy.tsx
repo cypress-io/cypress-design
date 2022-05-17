2cb8d86ea371ab2cb47b83beb2669702e302a3bf
Bart Ledoux <bart@cypress.io>
2022-05-17 11:56:43 -0500

    fix vue tests

--- components/Spinner/vue/Spinner.cy.tsx
+++ components/Spinner/vue/Spinner.cy.tsx
@@ -1,11 +1,13 @@
 /// <reference types="cypress" />
 
+import { mount } from 'cypress/vue';
+
 import Spinner from './Spinner.vue';
 
 describe('<Spinner />', () => {
   it('renders', () => {
-    cy.mount(
-      <Spinner label="Welcome guide settings" id="welcome-opt-out" />
+    mount(() => 
+      <Spinner />
     );
   });
 });
