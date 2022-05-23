7b31772614551984e574d1f1ae65ce402ccf416a
Bart Ledoux <bart@cypress.io>
2022-05-23 10:51:41 -0500

    extract ally support to its own file

---
+++ cypress/support/a11y-support.ts
@@ -0,0 +1,29 @@
+import 'cypress-axe';
+
+beforeEach(() => {
+  cy.injectAxe();
+  cy.configureAxe({
+    rules: [
+      {
+        id: 'html-has-lang',
+        enabled: false,
+      },
+      {
+        id: 'landmark-one-main',
+        enabled: false,
+      },
+      {
+        id: 'page-has-heading-one',
+        enabled: false,
+      },
+      {
+        id: 'region',
+        enabled: false,
+      },
+    ],
+  });
+});
+
+afterEach(() => {
+  cy.checkA11y();
+});
