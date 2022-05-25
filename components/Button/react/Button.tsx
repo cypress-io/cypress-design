9d98641b796be3d60f884f403ef88e4a523c8954
Shawn Taylor <shawn@cypress.io>
2022-05-25 17:34:41 -0400

    Make React button clickable

--- components/Button/react/Button.tsx
+++ components/Button/react/Button.tsx
@@ -1,33 +1,46 @@
 import clsx from 'clsx';
 import * as React from 'react';
 import type { ButtonSizes, ButtonVariants } from '../constants';
-import { SizeClassesTable, VariantClassesTable, StaticClasses } from '../constants';
+import {
+  SizeClassesTable,
+  VariantClassesTable,
+  StaticClasses,
+} from '../constants';
 
 export interface ButtonProps {
   variant?: ButtonVariants;
   size?: ButtonSizes;
   disabled?: boolean;
   className?: string;
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
-export const Button: React.FC<ButtonProps> = ({
+export const Button: React.FC<
+  ButtonProps & React.HTMLProps<HTMLButtonElement>
+> = ({
   variant = 'primary',
   size = 'md',
   disabled = false,
   className,
-  children
+  children,
+  ...rest
 }) => {
-  const finalVariant = (disabled && !['secondary', 'link'].includes(variant)) ? 'disabled' : variant
+  const finalVariant =
+    disabled && !['secondary', 'link'].includes(variant) ? 'disabled' : variant;
   const finalDisabled = disabled || variant === 'disabled';
   return (
-    <button className={clsx(
-      StaticClasses, 
-      VariantClassesTable[finalVariant], 
-      SizeClassesTable[size],
-      className)} 
-      disabled={finalDisabled}>
-        { children }
+    <button
+      {...rest}
+      type="button"
+      className={clsx(
+        StaticClasses,
+        VariantClassesTable[finalVariant],
+        SizeClassesTable[size],
+        className
+      )}
+      disabled={finalDisabled}
+    >
+      {children}
     </button>
   );
 };
--- components/Button/vue/Button.cy.tsx
+++ components/Button/vue/Button.cy.tsx
@@ -1,13 +1,20 @@
 /// <reference types="cypress" />
 import { mount } from 'cypress/vue';
+import Button from './Button.vue';
 import ButtonStory from './Button.rootstory';
 
-describe('<Button />', {viewportHeight:600, viewportWidth:1000}, () => {
+describe('<Button />', { viewportHeight: 600, viewportWidth: 1000 }, () => {
   it('renders variants disabled', () => {
     mount(() => ButtonStory({ disabled: true }));
   });
 
   it('renders variants', () => {
     mount(ButtonStory);
+  });
+
+  it('is clickable', () => {
+    const onClickSpy = cy.spy().as('onClickSpy');
+    mount(() => <Button onClick={onClickSpy}>Hello</Button>);
+    cy.get('button').click().get('@onClickSpy').should('have.been.calledOnce');
   });
 });
