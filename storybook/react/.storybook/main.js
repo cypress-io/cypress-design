697e804d5a3cfe491c611ec4ef7b83ca64b72b6e
Barthélémy Ledoux <ledouxb@gmail.com>
2022-05-25 11:45:32 -0500

    docs: add getting started page (#25)
    * remove intro and add getting started
    
    * repair intro pages
    
    * make getting started a tad simpler
    
    * make both indexes
    
    * make better navigation + pages
    
    * tighter copy
    
    * fix order of stories
    
    * update style and copy of getting started

--- storybook/react/.storybook/main.js
+++ storybook/react/.storybook/main.js
@@ -2,7 +2,10 @@ const { CyCSSWebpackPlugin } = require('@cypress-design/css');
 const path = require('path');
 
 module.exports = {
-  stories: ['../../../components/*/react/*.stories.@(mdx|tsx)'],
+  stories: [
+    '../**/*.stories.@(mdx|tsx)',
+    '../../../components/*/react/*.stories.@(mdx|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -22,10 +25,12 @@ module.exports = {
             ),
             path.resolve(__dirname, '../../../components/*/react/*.tsx'),
             path.resolve(__dirname, '../../../components/*/*.ts'),
+            path.resolve(__dirname, '../stories/**/*.mdx'),
           ],
         },
       })
     );
+    config.resolve.extensions.push('.json');
     return config;
   },
 };
--- storybook/vue/.storybook/preview.js
+++ storybook/vue/.storybook/preview.js
@@ -11,6 +11,7 @@ export const parameters = {
   options: {
     storySort: {
       method: 'alphabetical',
+      order: ['Index', '*'],
     },
   },
 };
