6a01c269d22a5e4c12d034db88ac33e6e0603887
Bart Ledoux <bart@cypress.io>
2022-05-31 16:47:47 -0500

    add a padding to the blocks to allow for interactive

--- components/Tooltip/react/Tooltip.tsx
+++ components/Tooltip/react/Tooltip.tsx
@@ -12,6 +12,7 @@ import {
   useFocus,
   useRole,
   useDismiss,
+  safePolygon,
 } from '@floating-ui/react-dom-interactions'
 
 export interface TooltipProps {
@@ -52,12 +53,16 @@ export const Tooltip: React.FC<TooltipProps> = ({
     placement,
     open,
     onOpenChange: setOpen,
-    middleware: [flip(), offset(16), arrow({ element: arrowRef, padding: 8 })],
+    middleware: [flip(), offset(0), arrow({ element: arrowRef, padding: 8 })],
     whileElementsMounted: autoUpdate,
   })
 
   const { getReferenceProps, getFloatingProps } = useInteractions([
-    useHover(context),
+    useHover(context, {
+      handleClose: safePolygon({
+        restMs: 50,
+      }),
+    }),
     useFocus(context),
     useRole(context, { role: 'tooltip' }),
     useDismiss(context),
@@ -94,45 +99,50 @@ export const Tooltip: React.FC<TooltipProps> = ({
               position: strategy,
               top: y ?? '',
               left: x ?? '',
+              padding: '16px',
             },
           })}
-          className={clsx(
-            'rounded shadow border p-8px text-16px leading-24px min-w-160px text-center',
-            [
-              color === 'dark' && 'bg-gray-900 shadow-gray-800 border-gray-800',
-              color === 'light' && 'bg-white shadow-gray-100 border-gray-100',
-            ]
-          )}
         >
-          <svg
-            ref={arrowRef}
-            viewBox="0 0 48 24"
-            width="24"
-            height="12"
-            className={clsx('absolute', {
-              'stroke-gray-800 fill-gray-900': color === 'dark',
-              'stroke-gray-100 fill-white': color === 'light',
-            })}
-            style={{
-              transform: `rotate(${arrowRotate}deg)`,
-              filter:
-                placementSide === 'bottom'
-                  ? undefined
-                  : color === 'dark'
-                  ? 'drop-shadow(0 0 2px rgba(30, 30, 30, 1))'
-                  : 'drop-shadow(0 1px 1px rgba(225, 227, 237, .8))',
-              [arrowXRule]: `${arrowX ?? -17}px`,
-              [arrowYRule]: `${arrowY ?? -11}px`,
-            }}
-            fill="none"
+          <div
+            className={clsx(
+              'rounded shadow border p-8px text-16px leading-24px min-w-160px text-center',
+              [
+                color === 'dark' &&
+                  'bg-gray-900 shadow-gray-800 border-gray-800',
+                color === 'light' && 'bg-white shadow-gray-100 border-gray-100',
+              ]
+            )}
           >
-            <rect x="0" y="0" width="48" height="4" strokeWidth="0" />
-            <path
-              d="M 0 3 C 12 3 18 18 24 18 C 30 18 36 3 48 3"
-              stroke-width="2"
-            />
-          </svg>
-          {popper}
+            <svg
+              ref={arrowRef}
+              viewBox="0 0 48 24"
+              width="24"
+              height="12"
+              className={clsx('absolute', {
+                'stroke-gray-800 fill-gray-900': color === 'dark',
+                'stroke-gray-100 fill-white': color === 'light',
+              })}
+              style={{
+                transform: `rotate(${arrowRotate}deg)`,
+                filter:
+                  placementSide === 'bottom'
+                    ? undefined
+                    : color === 'dark'
+                    ? 'drop-shadow(0 0 2px rgba(30, 30, 30, 1))'
+                    : 'drop-shadow(0 1px 1px rgba(225, 227, 237, .8))',
+                [arrowXRule]: `${arrowX ?? 0}px`,
+                [arrowYRule]: `${arrowY ?? 6}px`,
+              }}
+              fill="none"
+            >
+              <rect x="0" y="0" width="48" height="4" strokeWidth="0" />
+              <path
+                d="M 0 3 C 12 3 18 18 24 18 C 30 18 36 3 48 3"
+                stroke-width="2"
+              />
+            </svg>
+            {popper}
+          </div>
         </div>
       )}
     </>
--- components/Tooltip/vue/Tooltip.vue
+++ components/Tooltip/vue/Tooltip.vue
@@ -4,30 +4,33 @@
       <slot />
     </div>
     <div role="tooltip" ref="tooltip"
-      :style="positionComputed ? `top:${top}px!important;left:${left}px!important;` : undefined"
-      class="absolute rounded shadow border p-8px text-16px leading-24px min-w-160px text-center" :class="{
+      :style="positionComputed ? `top:${top}px!important;left:${left}px!important;` : undefined" class="absolute p-16px"
+      :class="{
         'hidden group-hover:block': positionComputed,
         '-top-10000px': !positionComputed,
+      }">
+      <div class="rounded shadow border p-8px text-16px leading-24px min-w-160px text-center" :class="{
         'bg-white border-gray-100 shadow-gray-100': props.color === 'light',
         'bg-gray-900 shadow-gray-800 border-gray-800': props.color === 'dark',
       }">
-      <svg ref="arrowRef" viewBox="0 0 48 24" width="24" height="12" class="absolute" :class="{
-        'stroke-gray-100 fill-white': props.color === 'light',
-        'stroke-gray-800 fill-gray-900': props.color === 'dark',
-      }"
-        :style="`transform: rotate(${arrowRotate}deg); filter: ${dropShadowFilter};${arrowYRule}:${arrowTop}px!important;${arrowXRule}:${arrowLeft}px!important;`"
-        fill="none">
-        <rect x=" 0" y="0" width="48" height="4" stroke-width="0" />
-        <path d="M 0 3 C 12 3 18 18 24 18 C 30 18 36 3 48 3" stroke-width="2" />
-      </svg>
-      <slot name="popper" />
+        <svg ref="arrowRef" viewBox="0 0 48 24" width="24" height="12" class="absolute" :class="{
+          'stroke-gray-100 fill-white': props.color === 'light',
+          'stroke-gray-800 fill-gray-900': props.color === 'dark',
+        }"
+          :style="`transform: rotate(${arrowRotate}deg); filter: ${dropShadowFilter};${arrowYRule}:${arrowTop}px!important;${arrowXRule}:${arrowLeft}px!important;`"
+          fill="none">
+          <rect x=" 0" y="0" width="48" height="4" stroke-width="0" />
+          <path d="M 0 3 C 12 3 18 18 24 18 C 30 18 36 3 48 3" stroke-width="2" />
+        </svg>
+        <slot name="popper" />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import type { Placement, Side } from '@floating-ui/dom';
-import { computePosition, flip, offset, arrow, shift } from '@floating-ui/dom';
+import { computePosition, flip, offset, arrow } from '@floating-ui/dom';
 import { ref, type Ref } from 'vue';
 
 const props = withDefaults(
@@ -67,7 +70,7 @@ async function placeTooltip() {
     placement: props.placement,
     middleware: [
       flip(),
-      offset(28),
+      offset(16),
       arrow({ element: arrowRef.value, padding: 8 })
     ],
   })
@@ -89,12 +92,12 @@ async function placeTooltip() {
     arrowYRule.value = 'top'
   } else if (arrowX) {
     arrowLeft.value = arrowX
-    arrowTop.value = -11
+    arrowTop.value = 6
     arrowXRule.value = 'left'
     arrowYRule.value = placementSide === 'top' ? 'bottom' : 'top'
   } else if (arrowY) {
     arrowTop.value = arrowY
-    arrowLeft.value = -17
+    arrowLeft.value = 0
     arrowXRule.value = placementSide === 'left' ? 'right' : 'left'
     arrowYRule.value = 'top'
   }
