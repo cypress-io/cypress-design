c24e62ac50b63b09e8476a9a2f823b96b8e6e596
Bart Ledoux <bart@cypress.io>
2022-06-03 21:01:24 -0500

    allow linting of vue components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
   plugins: ['@typescript-eslint', 'no-only-tests'],
   rules: {
     'no-console': 'error',
