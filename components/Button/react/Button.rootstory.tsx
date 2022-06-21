ff7a75aa44f6452e9041b9ec8e1d9c499106fcf1
Shawn Taylor <shawn@cypress.io>
2022-06-21 11:06:47 -0400

    [DESIGN-1162] Button updates (#38)
    * Add more button variants

--- components/Button/react/Button.rootstory.tsx
+++ components/Button/react/Button.rootstory.tsx
@@ -1,20 +1,33 @@
+import clsx from 'clsx'
 import * as React from 'react'
 import Button from './Button'
 import { SizeClassesTable, VariantClassesTable } from '../constants'
 import type { ButtonSizes, ButtonVariants } from '../constants'
 
 export default ({ disabled = false }: { disabled?: boolean } = {}) => (
-  <div className="flex flex-row flex-wrap gap-3 justify-center">
+  <div className="flex flex-row flex-wrap gap-2 justify-center">
     {(Object.keys(VariantClassesTable) as ButtonVariants[]).map((variant) => {
       return (
-        <div className="flex flex-col items-center gap-3 justify-center mt-4">
+        <div
+          className={clsx(
+            'flex flex-col items-center gap-3 justify-center mt-4 p-2',
+            { 'bg-gray-1000 text-white': variant === 'outline-dark' }
+          )}
+        >
           <h3 className="text-right">{variant}</h3>
           {(Object.keys(SizeClassesTable) as ButtonSizes[])
             .reverse()
             .map((size) => {
               return (
                 <div className="flex items-center justify-center">
-                  <span className="text-gray-700 text-sm mr-4">{size}</span>
+                  <span
+                    className={clsx('text-sm mr-4', {
+                      'text-gray-300': variant === 'outline-dark',
+                      'text-gray-700': variant !== 'outline-dark',
+                    })}
+                  >
+                    {size}
+                  </span>
                   <Button variant={variant} size={size} disabled={disabled}>
                     Button
                   </Button>
--- components/Button/vue/Button.rootstory.tsx
+++ components/Button/vue/Button.rootstory.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import Button from './Button.vue'
 import {
   SizeClassesTable,
@@ -10,14 +11,26 @@ export default ({ disabled = false }: { disabled?: boolean } = {}) => (
   <div class="flex flex-row flex-wrap gap-3 justify-center">
     {(Object.keys(VariantClassesTable) as ButtonVariants[]).map((variant) => {
       return (
-        <div class="flex flex-col items-center gap-3 justify-center mt-4">
+        <div
+          class={clsx(
+            'flex flex-col items-center gap-3 justify-center mt-4 p-2',
+            { 'bg-gray-1000 text-white': variant === 'outline-dark' }
+          )}
+        >
           <h3 class="text-right">{variant}</h3>
           {(Object.keys(SizeClassesTable) as ButtonSizes[])
             .reverse()
             .map((size) => {
               return (
                 <div class="flex items-center justify-center">
-                  <span class="text-gray-700 text-sm mr-4">{size}</span>
+                  <span
+                    class={clsx('text-sm mr-4', {
+                      'text-gray-300': variant === 'outline-dark',
+                      'text-gray-700': variant !== 'outline-dark',
+                    })}
+                  >
+                    {size}
+                  </span>
                   <Button variant={variant} size={size} disabled={disabled}>
                     Button
                   </Button>
