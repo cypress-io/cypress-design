e868cf976bd5e48970bdb53bebba79738cfec72a
Bart Ledoux <bart@cypress.io>
2022-06-28 18:02:17 -0500

    make comments remain on alert vue

--- components/Alert/vue/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import * as path from 'path'
-import generateViteConfig from '../../root.vite.config'
-
-export default generateViteConfig({
-  entry: path.resolve(__dirname, './index.ts'),
-  name: 'Alert',
-})
