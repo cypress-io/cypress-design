d7d2a670acdde7bdbbc1916b99116e648ad6f694
Bart Ledoux <bart@cypress.io>
2022-07-01 17:03:38 -0500

    fix plugin

--- css/src/icon-color-plugins.test.ts
+++ css/src/icon-color-plugins.test.ts
@@ -7,7 +7,7 @@ test('should generate correct css', () => {
     plugins: [IconDuotoneColorsPlugin],
   })
   const result = processor.interpret(
-    'icon-light-blue-500 hover-icon:icon-light-blue-700 focus-icon:icon-light-blue-700 icon-dark-blue-200 hocus-icon:icon-dark-blue-700'
+    'icon-light-blue-500 icon-hover:icon-light-blue-700 icon-focus:icon-light-blue-700 icon-dark-blue-200 icon-hocus:icon-dark-blue-700'
   )
   expect(result.ignored.length).toEqual(0)
   expect(result.styleSheet.build()).toMatchInlineSnapshot(`
@@ -35,40 +35,40 @@ test('should generate correct css', () => {
     .icon-light-blue-500 > *[fill][stroke].icon-light-stroke {
       stroke: #3b82f6;
     }
-    .hover-icon\\\\:icon-light-blue-700:hover > *[fill].icon-light {
+    .icon-hover\\\\:icon-light-blue-700:hover > *[fill].icon-light {
       fill: #1d4ed8;
     }
-    .focus-icon\\\\:icon-light-blue-700:focus > *[fill].icon-light {
+    .icon-focus\\\\:icon-light-blue-700:focus > *[fill].icon-light {
       fill: #1d4ed8;
     }
-    .hover-icon\\\\:icon-light-blue-700:hover > *[stroke].icon-light {
+    .icon-hover\\\\:icon-light-blue-700:hover > *[stroke].icon-light {
       stroke: #1d4ed8;
     }
-    .focus-icon\\\\:icon-light-blue-700:focus > *[stroke].icon-light {
+    .icon-focus\\\\:icon-light-blue-700:focus > *[stroke].icon-light {
       stroke: #1d4ed8;
     }
-    .hover-icon\\\\:icon-light-blue-700:hover > *[fill][stroke].icon-light-fill {
+    .icon-hover\\\\:icon-light-blue-700:hover > *[fill][stroke].icon-light-fill {
       fill: #1d4ed8;
     }
-    .focus-icon\\\\:icon-light-blue-700:focus > *[fill][stroke].icon-light-fill {
+    .icon-focus\\\\:icon-light-blue-700:focus > *[fill][stroke].icon-light-fill {
       fill: #1d4ed8;
     }
-    .hover-icon\\\\:icon-light-blue-700:hover > *[fill][stroke].icon-light-stroke {
+    .icon-hover\\\\:icon-light-blue-700:hover > *[fill][stroke].icon-light-stroke {
       stroke: #1d4ed8;
     }
-    .focus-icon\\\\:icon-light-blue-700:focus > *[fill][stroke].icon-light-stroke {
+    .icon-focus\\\\:icon-light-blue-700:focus > *[fill][stroke].icon-light-stroke {
       stroke: #1d4ed8;
     }
-    .hocus-icon\\\\:icon-dark-blue-700:hover > *[fill].icon-dark, .hocus-icon\\\\:icon-dark-blue-700:focus > *[fill].icon-dark {
+    .icon-hocus\\\\:icon-dark-blue-700:hover > *[fill].icon-dark, .icon-hocus\\\\:icon-dark-blue-700:focus > *[fill].icon-dark {
       fill: #1d4ed8;
     }
-    .hocus-icon\\\\:icon-dark-blue-700:hover > *[stroke].icon-dark, .hocus-icon\\\\:icon-dark-blue-700:focus > *[stroke].icon-dark {
+    .icon-hocus\\\\:icon-dark-blue-700:hover > *[stroke].icon-dark, .icon-hocus\\\\:icon-dark-blue-700:focus > *[stroke].icon-dark {
       stroke: #1d4ed8;
     }
-    .hocus-icon\\\\:icon-dark-blue-700:hover > *[fill][stroke].icon-dark-fill, .hocus-icon\\\\:icon-dark-blue-700:focus > *[fill][stroke].icon-dark-fill {
+    .icon-hocus\\\\:icon-dark-blue-700:hover > *[fill][stroke].icon-dark-fill, .icon-hocus\\\\:icon-dark-blue-700:focus > *[fill][stroke].icon-dark-fill {
       fill: #1d4ed8;
     }
-    .hocus-icon\\\\:icon-dark-blue-700:hover > *[fill][stroke].icon-dark-stroke, .hocus-icon\\\\:icon-dark-blue-700:focus > *[fill][stroke].icon-dark-stroke {
+    .icon-hocus\\\\:icon-dark-blue-700:hover > *[fill][stroke].icon-dark-stroke, .icon-hocus\\\\:icon-dark-blue-700:focus > *[fill][stroke].icon-dark-stroke {
       stroke: #1d4ed8;
     }"
   `)
