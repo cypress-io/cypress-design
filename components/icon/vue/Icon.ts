9cea8e6d1c9e3ec142e6587cb8bbdd45cab0a544
elevatebart <bart@cypress.io>
2022-08-05 16:47:19 -0500

    re-implement vue-icon

--- components/icon/vue/Icon.ts
+++ components/icon/vue/Icon.ts
@@ -1,11 +1,9 @@
 import { compileIcon } from '@cypress-design/icon-registry'
 import type { IconProps } from '@cypress-design/icon-registry'
-import { defineComponent, h } from 'vue'
+import { h } from 'vue'
 import type { SVGAttributes } from 'vue'
 import { compileVueIconProperties } from './compileProperties'
 
-export default defineComponent(
-  (props: IconProps & Omit<SVGAttributes, 'name'>) => {
-    return h('svg', compileVueIconProperties(compileIcon(props)))
-  }
-)
+export default (props: IconProps & Omit<SVGAttributes, 'name'>) => {
+  return h('svg', compileVueIconProperties(compileIcon(props)))
+}
