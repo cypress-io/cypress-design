e1e81c316ea29342346c0e5dfa85d8ad2d23372d
Shawn Taylor <shawn@cypress.io>
2022-08-09 13:25:59 -0400

    Add a test

--- components/checkbox/react/CheckboxReact.cy.tsx
+++ components/checkbox/react/CheckboxReact.cy.tsx
@@ -3,7 +3,7 @@ import { mount } from 'cypress/react'
 import { Checkbox } from './Checkbox'
 
 describe('Checkbox', () => {
-  it('renders', () => {
+  it('changes when the label is clicked', () => {
     let isChecked = true
 
     mount(
@@ -18,6 +18,24 @@ describe('Checkbox', () => {
 
     cy.get('input[type="checkbox"]').should('be.checked')
     cy.contains('Welcome guide settings').click()
+    cy.get('input[type="checkbox"]').should('not.be.checked')
+  })
+
+  it('changes when checkbox is clicked', () => {
+    let isChecked = true
+
+    mount(
+      <Checkbox
+        label="Welcome guide settings"
+        id="welcome-opt-out"
+        checked={isChecked}
+        onChange={() => (isChecked = !isChecked)}
+        className="m-2 px-2 py-1 border-1 border-gray-300 rounded"
+      />
+    )
+
+    cy.get('input[type="checkbox"]').should('be.checked')
+    cy.get('svg').click()
     cy.get('input[type="checkbox"]').should('not.be.checked')
   })
 })
--- components/checkbox/vue/CheckboxVue.cy.tsx
+++ components/checkbox/vue/CheckboxVue.cy.tsx
@@ -5,7 +5,7 @@ import { mount } from 'cypress/vue'
 import Checkbox from './Checkbox.vue'
 
 describe('<Checkbox />', () => {
-  it('renders', () => {
+  it('changes when the label is clicked', () => {
     mount(() => {
       return (
         <Checkbox
@@ -19,6 +19,23 @@ describe('<Checkbox />', () => {
 
     cy.get('input[type="checkbox"]').should('be.checked')
     cy.contains('Welcome guide settings').click()
+    cy.get('input[type="checkbox"]').should('not.be.checked')
+  })
+
+  it('changes when checkbox is clicked', () => {
+    mount(() => {
+      return (
+        <Checkbox
+          label="Welcome guide settings"
+          id="welcome-opt-out"
+          checked
+          class="m-2 px-2 py-1 border-1 border-gray-300 rounded"
+        />
+      )
+    })
+
+    cy.get('input[type="checkbox"]').should('be.checked')
+    cy.get('svg').click()
     cy.get('input[type="checkbox"]').should('not.be.checked')
   })
 
