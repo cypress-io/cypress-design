cea5f9c9d2bb582d0440ccaf0e6432c0df70a742
elevatebart <bart@cypress.io>
2022-08-10 09:28:55 -0500

    add tests

--- components/Accordion/assertions.ts
+++ components/Accordion/assertions.ts
@@ -1,6 +1,17 @@
 /// <reference types="cypress" />
 
-export default function assertions(mountStory: (options?: any) => void): void {
+export interface AccordionStoryOptions {
+  title?: string
+  description?: string
+  icon?: any
+  separator?: boolean
+  open?: boolean
+  fullWidthContent?: boolean
+}
+
+export default function assertions(
+  mountStory: (options?: AccordionStoryOptions) => void
+): void {
   it('opens when clicking on the heading', () => {
     mountStory()
     cy.get('details').click()
@@ -17,5 +28,23 @@ export default function assertions(mountStory: (options?: any) => void): void {
     cy.get('details').click()
 
     cy.contains('Lorem ipsum, dolor sit amet').should('not.be.visible')
+  })
+
+  it('displays a separator when separator:true', () => {
+    mountStory({ separator: true })
+    cy.get('hr').should('be.visible')
+  })
+
+  it('not display a separator when separator:false', () => {
+    mountStory({ separator: false })
+    cy.get('hr').should('not.exist')
+  })
+
+  it('have no padding when fullwidth', () => {
+    mountStory({ fullWidthContent: true })
+
+    cy.get('details').click()
+
+    cy.get('[data-cy="content"]').parent().should('have.css', 'padding', '0px')
   })
 }
