a04934f3fb7ab752877f0ca9618e4558066bf193
Barthélémy Ledoux <ledouxb@gmail.com>
2022-08-11 13:45:28 -0500

    refactor: remove need for specific css when details (#75)
    * fix: remove need for specific css when details
    
    * add animation to details
    
    * make rotate triangle work
    
    * update alert process
    
    * add changeset

---
+++ .changeset/fair-taxis-collect.md
@@ -0,0 +1,6 @@
+---
+'@cypress-design/react-alert': patch
+'@cypress-design/vue-alert': patch
+---
+
+refactor to remove need for custom css
--- components/Alert/vue/Alert.vue
+++ components/Alert/vue/Alert.vue
@@ -1,15 +1,28 @@
 <template>
-  <div v-if="!dismissed" class="overflow-hidden text-left" :class="props.notRounded ? undefined : 'rounded'">
+  <div
+    v-if="!dismissed"
+    class="overflow-hidden text-left"
+    :class="props.notRounded ? undefined : 'rounded'"
+  >
     <div class="flex p-16px" :class="typeClasses.headerClass">
       <!-- @slot replace the default left icon here -->
       <slot name="icon" v-bind="computedIconProps">
-        <component v-if="!props.noIcon && icon" :is="icon" v-bind="computedIconProps" />
+        <component
+          v-if="!props.noIcon && icon"
+          :is="icon"
+          v-bind="computedIconProps"
+        />
       </slot>
       <div class="flex-1 font-medium">
         <!-- @slot title of the alert -->
         <slot />
       </div>
-      <button v-if="dismissible" class="m-4px ml-8px h-16px" @click="dismiss" aria-label="Dismiss">
+      <button
+        v-if="dismissible"
+        class="m-4px ml-8px h-16px"
+        @click="dismiss"
+        aria-label="Dismiss"
+      >
         <IconActionDeleteLarge :stroke-color="typeClasses.iconCloseColor" />
       </button>
     </div>
@@ -17,26 +30,42 @@
       <!-- @slot body/details of the alert -->
       <slot name="body" />
     </div>
-    <details v-if="slots.details" class="p-16px border-t-1" :class="[typeClasses.bodyClass, typeClasses.borderClass]">
-      <summary class="flex font-medium cursor-pointer" :class="typeClasses.detailsHeaderClass">
-        <IconChevronDownSmall :strokeColor="typeClasses.iconChevronColor" class="icon my-4px mr-8px" />
+    <details
+      v-if="slots.details"
+      class="p-16px border-t-1"
+      ref="detailsRef"
+      :class="[typeClasses.bodyClass, typeClasses.borderClass]"
+    >
+      <summary
+        class="flex font-medium cursor-pointer details-none"
+        :class="typeClasses.detailsHeaderClass"
+      >
+        <IconChevronDownSmall
+          :strokeColor="typeClasses.iconChevronColor"
+          class="icon my-4px mr-8px transition transform -rotate-90 open:rotate-0"
+        />
         {{ props.detailsTitle }}
       </summary>
-      <div class="mt-16px">
-        <!--@slot Togglable additional details-->
-        <slot name="details" />
+      <div ref="contentRef">
+        <div class="mt-16px">
+          <!--@slot Togglable additional details-->
+          <slot name="details" />
+        </div>
       </div>
     </details>
   </div>
 </template>
 
 <script lang="ts" setup>
-import type { FunctionalComponent, ComputedRef } from 'vue';
+import type { FunctionalComponent, ComputedRef } from 'vue'
 import { computed, useSlots, h, ref, onMounted, onUnmounted } from 'vue'
 import {
-  IconChevronDownSmall, IconActionDeleteLarge,
-  IconWarningCircle, IconCheckmarkOutline,
+  IconChevronDownSmall,
+  IconActionDeleteLarge,
+  IconWarningCircle,
+  IconCheckmarkOutline,
 } from '@cypress-design/vue-icon'
+import { DetailsAnimation } from '@cypress-design/details-animation'
 import type { AlertType } from '../constants'
 import { alertClasses } from '../constants'
 
@@ -48,38 +77,44 @@ const emit = defineEmits<{
   /**
    * Clicking on the dismiss button or dismissed after a timeout
    */
-  (event: 'dismiss'): void,
+  (event: 'dismiss'): void
 }>()
 
-const props = withDefaults(defineProps<{
-  /**
-   * Color scheme
-   */
-  type?: AlertType
-  /**
-   * If details are provided,text used in the toggle button
-   */
-  detailsTitle?: string
-  /**
-   * Show the dismiss button
-   */
-  dismissible?: boolean
-  /**
-   * When an icon is displayed by default, use this to remove it
-   */
-  noIcon?: boolean
-  /**
-   * If you need square corners
-   */
-  notRounded?: boolean
-  /**
-   * Dismiss the alert after a delay (in ms)
-   */
-  duration?: number
-}>(), {
-  type: 'info',
-  detailsTitle: 'Additional details',
-})
+const props = withDefaults(
+  defineProps<{
+    /**
+     * Color scheme
+     */
+    type?: AlertType
+    /**
+     * If details are provided,text used in the toggle button
+     */
+    detailsTitle?: string
+    /**
+     * Show the dismiss button
+     */
+    dismissible?: boolean
+    /**
+     * When an icon is displayed by default, use this to remove it
+     */
+    noIcon?: boolean
+    /**
+     * If you need square corners
+     */
+    notRounded?: boolean
+    /**
+     * Dismiss the alert after a delay (in ms)
+     */
+    duration?: number
+  }>(),
+  {
+    type: 'info',
+    detailsTitle: 'Additional details',
+  }
+)
+
+const detailsRef = ref(null)
+const contentRef = ref(null)
 
 const typeClasses = computed(() => {
   return alertClasses[props.type]
@@ -90,6 +125,12 @@ let timeout: number | undefined
 onMounted(() => {
   if (props.duration) {
     timeout = setTimeout(dismiss, props.duration) as any
+  }
+})
+
+onMounted(() => {
+  if (detailsRef.value && contentRef.value) {
+    new DetailsAnimation(detailsRef.value, contentRef.value)
   }
 })
 
@@ -111,14 +152,14 @@ function dismiss() {
 const computedIconProps = computed(() => {
   return {
     strokeColor: alertClasses[props.type].iconColor,
-    class: 'my-4px mr-8px'
+    class: 'my-4px mr-8px',
   }
 })
 
 const icon: ComputedRef<FunctionalComponent | null> = computed(() => {
   switch (props.type) {
     case 'info':
-      return null;
+      return null
     case 'success':
       return () => h(IconCheckmarkOutline, computedIconProps.value)
     case 'warning':
@@ -126,23 +167,7 @@ const icon: ComputedRef<FunctionalComponent | null> = computed(() => {
     case 'error':
       return () => h(IconWarningCircle, computedIconProps.value)
     default:
-      return null;
+      return null
   }
 })
-
 </script>
-
-<style scoped>
-details :deep(summary::-webkit-details-marker) {
-  display: none;
-}
-
-details summary .icon {
-  transform: rotate(-90deg);
-  transition: transform 0.2s;
-}
-
-details[open] summary .icon {
-  transform: rotate(0);
-}
-</style>
