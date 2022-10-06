99cb278869d2c235c7551ddafede7619cdfbb4d2
elevatebart <bart@cypress.io>
2022-10-06 10:02:16 -0500

    reset tests for vue

--- components/Tooltip/vue/TooltipVue.cy.tsx
+++ components/Tooltip/vue/TooltipVue.cy.tsx
@@ -1,8 +1,6 @@
 /// <reference types="cypress" />
-import { IconActionQuestionMarkCircle } from '@cypress-design/vue-icon'
 import { mount } from 'cypress/vue'
 import TooltipStory from './Tooltip.rootstory'
-import Tooltip from './Tooltip.vue'
 import assertions from '../assertions'
 
 describe('<Tooltip />', { viewportHeight: 800, viewportWidth: 800 }, () => {
