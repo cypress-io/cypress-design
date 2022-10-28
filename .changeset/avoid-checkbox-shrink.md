d229b48a3806a882abe83c9ebf9c32ed18d2c47f
Barthélémy Ledoux <ledouxb@gmail.com>
2022-10-28 10:45:00 -0500

    fix avoid checkbox shrinking when label becomes too big (flex side effect) (#112)
    * fix the sizing problem
    
    * use only pixels for dimension
    
    * same for react
    
    * add changeset

---
+++ .changeset/avoid-checkbox-shrink.md
@@ -0,0 +1,6 @@
+---
+'@cypress-design/react-checkbox': patch
+'@cypress-design/vue-checkbox': patch
+---
+
+fix: when label is too big avoid checkbox shrinking
--- components/checkbox/react/CheckboxReact.cy.tsx
+++ components/checkbox/react/CheckboxReact.cy.tsx
@@ -38,4 +38,16 @@ describe('Checkbox', () => {
     cy.get('svg').click()
     cy.get('input[type="checkbox"]').should('not.be.checked')
   })
+
+  it('keeps its width when label is long', () => {
+    mount(
+      <Checkbox
+        label="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        id="lorem-checkbox"
+        onChange={() => {}}
+      />
+    )
+
+    cy.get('label span:first-child').invoke('outerWidth').should('equal', 16)
+  })
 })
