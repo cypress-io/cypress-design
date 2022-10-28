cf9cc7eb8402c9413209963f3d74f6b53261ebd0
elevatebart <bart@cypress.io>
2022-10-27 20:38:08 -0500

    fix the sizing problem

--- components/checkbox/vue/CheckboxVue.cy.tsx
+++ components/checkbox/vue/CheckboxVue.cy.tsx
@@ -39,6 +39,19 @@ describe('<Checkbox />', () => {
     cy.get('input[type="checkbox"]').should('not.be.checked')
   })
 
+  it('keeps its width when label is long', () => {
+    mount(() => {
+      return (
+        <Checkbox
+          label="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          id="lorem-checkbox"
+        />
+      )
+    })
+
+    cy.get('label span:first-child').invoke('outerWidth').should('equal', 16)
+  })
+
   it('renders vModel', () => {
     const isChecked = ref(false)
     mount(() => {
