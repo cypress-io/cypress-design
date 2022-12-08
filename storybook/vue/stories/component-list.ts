202afaf70b5185ef5a94a07005a437fe085af4fe
Shawn Taylor <shawn@cypress.io>
2022-12-08 12:19:10 -0500

    Fix docs links for components with names that are not all lowercase

--- storybook/vue/stories/component-list.ts
+++ storybook/vue/stories/component-list.ts
@@ -1,3 +1,13 @@
 const comps = import.meta.globEager('../../../components/*/vue/package.json')
 
+export const capName = Object.keys(comps).reduce((acc, fullPath) => {
+  const cleanComponentName = fullPath
+    .replace('../../../components/', '')
+    .replace('/vue/package.json', '')
+
+  acc[cleanComponentName.toLowerCase()] = cleanComponentName
+
+  return acc
+}, {})
+
 export default comps
