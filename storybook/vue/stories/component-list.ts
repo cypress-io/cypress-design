c3e86730f579e03791d296c6349abd5828265625
Shawn Taylor <shawn@cypress.io>
2022-12-08 13:31:04 -0500

    Rename const

--- storybook/vue/stories/component-list.ts
+++ storybook/vue/stories/component-list.ts
@@ -1,13 +1,16 @@
 const comps = import.meta.globEager('../../../components/*/vue/package.json')
 
-export const capName = Object.keys(comps).reduce((acc, fullPath) => {
-  const cleanComponentName = fullPath
-    .replace('../../../components/', '')
-    .replace('/vue/package.json', '')
+export const capitalizedComponentNameMap = Object.keys(comps).reduce(
+  (acc, fullPath) => {
+    const cleanComponentName = fullPath
+      .replace('../../../components/', '')
+      .replace('/vue/package.json', '')
 
-  acc[cleanComponentName.toLowerCase()] = cleanComponentName
+    acc[cleanComponentName.toLowerCase()] = cleanComponentName
 
-  return acc
-}, {})
+    return acc
+  },
+  {}
+)
 
 export default comps
