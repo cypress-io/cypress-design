a50ae9d86a5b6f0555db5f2e29a26221878c3af9
Bart Ledoux <ledouxb@gmail.com>
2022-12-17 15:12:25 -0600

    add tailwind to css and a test project

---
+++ test/vue-app-tailwind/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+function defineConfig(config: Config): Config {
+  return config
+}
+
+export default defineConfig({
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
