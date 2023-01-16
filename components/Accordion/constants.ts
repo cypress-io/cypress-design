a24a413581db72fb30945ec82ac2d5752c854e3e
elevatebart <bart@cypress.io>
2023-01-16 14:08:07 -0600

    fix: remove accordion height

--- components/Accordion/constants.ts
+++ components/Accordion/constants.ts
@@ -1,6 +1,7 @@
 export const CssClasses = {
-  summary: 'details-none card open:rounded-b-none min-h-65px',
-  summaryDiv: 'flex flex-row py-12px px-24px flex-nowrap items-center',
+  summary: 'details-none card open:rounded-b-none',
+  summaryDiv:
+    'flex flex-row py-12px px-24px flex-nowrap items-center min-h-63px',
   summaryColor: 'bg-white',
   summaryTitle: 'text-18px leading-28px font-medium',
   summaryTitleColor: 'text-indigo-500',
