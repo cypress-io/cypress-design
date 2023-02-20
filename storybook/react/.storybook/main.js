e8a483d06abe1bb82822917212a7e71e5545a9da
elevatebart <bart@cypress.io>
2023-02-20 10:16:53 -0600

    add addon postcss to sb-react

--- storybook/react/.storybook/main.js
+++ storybook/react/.storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
     '@storybook/preset-scss',
     'storybook-addon-designs',
   ],
