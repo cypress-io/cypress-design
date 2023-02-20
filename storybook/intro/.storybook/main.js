49b40703d139c47e2b49f4b3ff2bf3130673445f
elevatebart <bart@cypress.io>
2023-02-20 08:52:51 -0600

    remove vue-tailwind

--- storybook/intro/.storybook/main.js
+++ storybook/intro/.storybook/main.js
@@ -24,11 +24,6 @@ module.exports = {
           url: 'http://localhost:9996',
           expanded: true,
         },
-        vueTailwind: {
-          title: 'Vue Tailwind',
-          url: 'http://localhost:9994',
-          expanded: true,
-        },
       }
     }
     return {
@@ -40,11 +35,6 @@ module.exports = {
       vue: {
         title: 'Vue',
         url: '/vue',
-        expanded: true,
-      },
-      vueTailwind: {
-        title: 'Vue Tailwind',
-        url: '/vue-tailwind',
         expanded: true,
       },
     }
