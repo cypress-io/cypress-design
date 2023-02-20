e658befd9df8f55fcb79b4eeea5e4043deac9b20
elevatebart <bart@cypress.io>
2023-02-20 09:04:56 -0600

    snapshot alert

--- components/Alert/vue/AlertVue.cy.tsx
+++ components/Alert/vue/AlertVue.cy.tsx
@@ -5,5 +5,6 @@ import AlertStory from './Alert.rootstory'
 describe('<Alert />', { viewportHeight: 800 }, () => {
   it('renders', () => {
     mount(() => <AlertStory />)
+    cy.percySnapshot()
   })
 })
