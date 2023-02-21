45cbebe25f63d84a02285a40d32a7d5980e35a9a
elevatebart <bart@cypress.io>
2023-02-21 14:34:33 -0600

    make vue storybook build

--- storybook/vue/postcss.config.mjs
+++ storybook/vue/postcss.config.mjs
@@ -1,6 +1,6 @@
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
-import tailwindConfig from './tailwind.config'
+import tailwindConfig from './tailwind.config.mjs'
 
 export default {
   plugins: [
