1438f77b18bef4b794aa2a36ebdee499924c5512
elevatebart <bart@cypress.io>
2023-02-22 12:33:58 -0600

    add safelist of colors

--- storybook/intro/tailwind.config.cjs
+++ storybook/intro/tailwind.config.cjs
@@ -1,6 +1,38 @@
+// @ts-check
 const { TailwindConfig, TailwindIconExtractor } = require('@cypress-design/css')
 const { colors } = require('@cypress-design/css')
 const { map, reduce, kebabCase } = require('lodash')
+
+const safeColors = reduce(
+  { ...colors, transparent: { ONLY: true }, current: { ONLY: true } },
+  (acc, variants, colorName) => {
+    const name = kebabCase(colorName)
+
+    const colorVariants = map(variants, (_, k) => {
+      if (k === 'DEFAULT') return []
+      const variantName = k === 'ONLY' ? name : `${name}-${k}`
+      return [
+        `bg-${variantName}`,
+        `text-${variantName}`,
+        `before:bg-${variantName}`,
+        `before:text-${variantName}`,
+        `icon-light-${variantName}`,
+        `icon-dark-${variantName}`,
+        `icon-light-secondary-${variantName}`,
+        `icon-dark-secondary-${variantName}`,
+      ]
+    })
+
+    colorVariants.forEach((variant) => {
+      variant.forEach((v) => {
+        acc.push(v)
+      })
+    })
+
+    return acc
+  },
+  []
+)
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -12,25 +44,5 @@ module.exports = {
       {}
     ),
   },
-  safelist: reduce(
-    { ...colors, transparent: { ONLY: true }, current: { ONLY: true } },
-    (acc, variants, colorName) => {
-      const name = kebabCase(colorName)
-
-      return `${acc}
-			${map(variants, (_, k) => {
-        if (k === 'DEFAULT') return ``
-        const variantName = k === 'ONLY' ? name : `${name}-${k}`
-        return `
-					bg-${variantName}
-					text-${variantName}
-					before:bg-${variantName}
-					before:text-${variantName}
-					icon-light-${variantName}
-					icon-dark-${variantName}
-					icon-light-secondary-${variantName}
-					icon-dark-secondary-${variantName}`
-      }).join(' ')}`
-    }
-  ),
+  safelist: safeColors,
 }
