7585e2c0e4b834ae5bb5b60db095d30eec2a3ddf
elevatebart <bart@cypress.io>
2023-02-28 21:28:32 -0600

    add ring offset 0

--- components/Button/constants.ts
+++ components/Button/constants.ts
@@ -1,35 +1,35 @@
 export const VariantClassesTable = {
   link: 'border-transparent text-indigo-600 hover:underline disabled:text-gray-500 disabled:no-underline ring-transparent',
   'outline-indigo':
-    'border-indigo-500 text-indigo-500 hover:ring-indigo-300/35 disabled:text-gray-500',
+    'border-indigo-500 ring-offset-0 text-indigo-500 hover:ring-indigo-300/35 disabled:text-gray-500',
   'outline-purple':
-    'border-purple-500 text-purple-500 hover:ring-purple-100 disabled:text-gray-500',
+    'border-purple-500 ring-offset-0 text-purple-500 hover:ring-purple-100 disabled:text-gray-500',
   'outline-gray':
-    'border-gray-1000 text-gray-1000 hover:ring-gray-100 disabled:text-gray-500',
+    'border-gray-1000 ring-offset-0 text-gray-1000 hover:ring-gray-100 disabled:text-gray-500',
   'outline-light':
-    'border-gray-100 text-indigo-500 hover:border-gray-200 disabled:border-gray-100 hover:ring-gray-50 disabled:text-gray-500',
+    'border-gray-100 ring-offset-0 text-indigo-500 hover:border-gray-200 disabled:border-gray-100 hover:ring-gray-50 disabled:text-gray-500',
   'outline-dark':
-    'border-white/20 text-white hover:border-white/60 hover:ring-white/20 disabled:border-white/20 disabled:text-white/50',
+    'border-white/20 ring-offset-0 text-white hover:border-white/60 hover:ring-white/20 disabled:border-white/20 disabled:text-white/50',
   white:
-    'border-gray-100 bg-white text-indigo-500 hover:border-gray-200 hover:ring-gray-50 disabled:text-gray-500',
+    'border-gray-100 ring-offset-0 bg-white text-indigo-500 hover:border-gray-200 hover:ring-gray-50 disabled:text-gray-500',
   'indigo-light':
-    'border-indigo-50 bg-indigo-50 text-indigo-500 hover:border-indigo-200 hover:ring-2 hover:ring-indigo-50',
+    'border-indigo-50 ring-offset-0 bg-indigo-50 text-indigo-500 hover:border-indigo-200 hover:ring-2 hover:ring-indigo-50',
   'indigo-dark':
-    'border-indigo-500 bg-indigo-500 text-white hover:ring-2 hover:ring-indigo-100 focus:bg-indigo-600',
+    'border-indigo-500 ring-offset-0 bg-indigo-500 text-white hover:ring-2 hover:ring-indigo-100 focus:bg-indigo-600',
   'jade-light':
-    'border-transparent bg-jade-200 text-teal-600 hover:border-jade-300 hover:ring-2 hover:ring-jade-50',
+    'border-transparent ring-offset-0 bg-jade-200 text-teal-600 hover:border-jade-300 hover:ring-2 hover:ring-jade-50',
   'jade-dark':
-    'border-jade-500 bg-jade-500 text-white hover:ring-2 hover:ring-jade-100',
+    'border-jade-500 ring-offset-0 bg-jade-500 text-white hover:ring-2 hover:ring-jade-100',
   'teal-dark':
-    'border-teal-500 bg-teal-500 text-white hover:ring-2 hover:ring-teal-100 focus:bg-teal-600',
+    'border-teal-500 ring-offset-0 bg-teal-500 text-white hover:ring-2 hover:ring-teal-100 focus:bg-teal-600',
   'purple-dark':
-    'border-purple-500 bg-purple-500 text-white hover:ring-2 hover:ring-purple-100 focus:bg-purple-600',
+    'border-purple-500 ring-offset-0 bg-purple-500 text-white hover:ring-2 hover:ring-purple-100 focus:bg-purple-600',
   'red-dark':
-    'border-red-500 bg-red-500 text-white hover:ring-2 hover:ring-red-100 focus:bg-red-600',
+    'border-red-500 ring-offset-0 bg-red-500 text-white hover:ring-2 hover:ring-red-100 focus:bg-red-600',
   'gray-dark':
-    'border-gray-800 bg-gray-800 text-white hover:ring-2 hover:ring-gray-100',
+    'border-gray-800 ring-offset-0 bg-gray-800 text-white hover:ring-2 hover:ring-gray-100',
   'gray-darkest':
-    'border-gray-1000 bg-gray-1000 text-white hover:ring-2 hover:ring-gray-100',
+    'border-gray-1000 ring-offset-0 bg-gray-1000 text-white hover:ring-2 hover:ring-gray-100',
   disabled: 'border-gray-500 bg-gray-500 text-white',
 } as const
 
