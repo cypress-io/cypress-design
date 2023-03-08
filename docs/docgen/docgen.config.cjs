68cbcc79e2b2962bb561c96b36659a8b9b96f671
elevatebart <bart@cypress.io>
2023-03-08 10:41:41 -0600

    add ignore rootstory & cy

--- docs/docgen/docgen.config.cjs
+++ docs/docgen/docgen.config.cjs
@@ -1,5 +1,5 @@
 /// @ts-check
-const { defineConfig, defaultTemplates } = require('vue-docgen-cli')
+const { defineConfig } = require('vue-docgen-cli')
 const reactConfig = require('./docgen.config.react.cjs')
 const vueConfig = require('./docgen.config.vue.cjs')
 const props = require('./docgen.props.template.cjs')
@@ -11,6 +11,6 @@ module.exports = defineConfig({
   templates: {
     props,
   },
-
+  ignore: ['**/*.rootstory.tsx', '**/*.cy.tsx'],
   pages: [reactConfig, vueConfig],
 })
