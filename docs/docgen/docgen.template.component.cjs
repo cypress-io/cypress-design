462e328b88a29ed8c72520083e88d198c6a04b87
elevatebart <bart@cypress.io>
2023-03-14 13:14:10 -0500

    avoid conflicting with naturally registred components

--- docs/docgen/docgen.template.component.cjs
+++ docs/docgen/docgen.template.component.cjs
@@ -4,12 +4,13 @@ const { defaultTemplates } = require('vue-docgen-cli')
 /** @type typeof import('vue-docgen-cli').defaultTemplates.component */
 module.exports = function (...args) {
   const [, doc, , fileName] = args
+  const tempName = `__${doc.displayName}__`
   return `
 <script lang="ts" setup>
-import { ${doc.exportName} as ${
-    doc.displayName
-  } } from '../../../components/${fileName}'
-const components$ = { ${doc.displayName} }
+import { ${
+    doc.exportName
+  } as ${tempName} } from '../../../components/${fileName}'
+const components$ = { ${doc.displayName}: ${tempName} }
 </script>
 ${defaultTemplates.component(...args)}`
 }
