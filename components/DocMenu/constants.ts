67a1b9a0fec6db2218e3b48c0443e9934139329d
elevatebart <bart@cypress.io>
2023-03-16 13:14:38 -0500

    various fixes

--- components/DocMenu/constants.ts
+++ components/DocMenu/constants.ts
@@ -12,6 +12,6 @@ export interface NavGroup {
 
 export const classes = {
   button: 'flex leading-[24px] py-[8px] items-center relative',
-  topButton: 'leading-[24px] text-[16px] pl-[24px]',
+  topButton: 'leading-[24px] text-[16px] pl-[24px] font-medium',
   leafButton: 'leading-[20px] text-[14px] pl-[40px]',
 } as const
--- docs/.vitepress/theme/components/CommonSidebar.vue
+++ docs/.vitepress/theme/components/CommonSidebar.vue
@@ -1,21 +1,23 @@
 <script lang="ts" setup>
-import DocMenu from '@cypress-design/vue-docmenu'
+import { computed } from 'vue'
 
 const docsPages = import.meta.glob('../../../*.md', {
   eager: true,
 })
 
-const items = Object.keys(docsPages)
-  .map((p) => {
-    const route = p.replace(/^\.\.\/\.\.\/\.\.\//, '').replace(/\.md$/, '')
-    const name = route.split('/').pop() ?? ''
-    return {
-      text: name,
-      href: route,
-      active: route.includes(props.routePath),
-    }
-  })
-  .filter((p) => p.text.toLowerCase() !== 'index')
+const items = computed(() =>
+  Object.keys(docsPages)
+    .map((p) => {
+      const route = p.replace(/^\.\.\/\.\.\/\.\./, '').replace(/\.md$/, '')
+
+      return {
+        text: route.split('/').pop()?.replace(/-/g, ' ') ?? '',
+        href: route,
+        active: props.routePath.includes(route),
+      }
+    })
+    .filter((p) => p.text.toLowerCase() !== 'index')
+)
 
 const props = defineProps<{
   routePath: string
@@ -23,5 +25,16 @@ const props = defineProps<{
 </script>
 
 <template>
-  <DocMenu :group="{ items }" />
+  <ul>
+    <li v-for="item in items" :key="item.href" class="pl-[32px]">
+      <a
+        :href="item.href"
+        class="font-medium group relative inline-block py-[12px] leading-[24px] text-[16px] pl-[24px]"
+        :class="{
+          'text-indigo-500': item.active,
+        }"
+        >{{ item.text }}</a
+      >
+    </li>
+  </ul>
 </template>
