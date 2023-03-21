674f22c9a37f9d380d69d40748c8befe50fafa4c
elevatebart <bart@cypress.io>
2023-03-21 11:57:22 -0500

    fix component parsing path

--- docs/docgen/docgen.config.vue.cjs
+++ docs/docgen/docgen.config.vue.cjs
@@ -13,53 +13,63 @@ module.exports = defineConfig({
     const name = componentPath.split('/').pop() || 'unknown'
     return path.join(outDir, 'vue', name.replace(/\.(vue|ts)$/, '.md'))
   },
-  propsParser(componentPath) {
-    const exportNames = checker.getExportNames(componentPath)
-    return Promise.resolve(
-      exportNames.map((exportName) => {
-        const meta = checker.getComponentMeta(componentPath)
+  propsParser(componentPath, _, event) {
+    if (event === 'add') {
+      checker.reload()
+    }
+    return new Promise((resolve, reject) => {
+      try {
+        const exportNames = checker.getExportNames(componentPath)
+        resolve(
+          exportNames.map((exportName) => {
+            const meta = checker.getComponentMeta(componentPath)
 
-        const nonGlobalProps = meta.props.filter((prop) => !prop.global)
+            const nonGlobalProps = meta.props.filter((prop) => !prop.global)
 
-        // massage the output of meta to match the docgen format
-        const props = nonGlobalProps.length
-          ? nonGlobalProps.map((p) => ({
-              ...p,
-              type: {
-                name: p.type,
-              },
-              tags: p.tags.reduce((acc, t) => {
-                acc[t.name] = [{ title: t.name, content: t.text }]
-                return acc
-              }, {}),
-            }))
-          : undefined
+            // massage the output of meta to match the docgen format
+            const props = nonGlobalProps.length
+              ? nonGlobalProps.map((p) => ({
+                  ...p,
+                  type: {
+                    name: p.type,
+                  },
+                  tags: p.tags.reduce((acc, t) => {
+                    acc[t.name] = [{ title: t.name, content: t.text }]
+                    return acc
+                  }, {}),
+                }))
+              : undefined
 
-        const slots = meta.slots.length
-          ? meta.slots.map((s) => ({
-              name: s.name,
-            }))
-          : undefined
+            const slots = meta.slots.length
+              ? meta.slots.map((s) => ({
+                  name: s.name,
+                }))
+              : undefined
 
-        const events = meta.events.length
-          ? meta.events.map((s) => ({
-              name: s.name,
-            }))
-          : undefined
+            const events = meta.events.length
+              ? meta.events.map((s) => ({
+                  name: s.name,
+                }))
+              : undefined
 
-        return {
-          props,
-          slots,
-          events,
-          displayName:
-            componentPath
-              .split('/')
-              .pop()
-              ?.replace(/\.(ts|js|vue)/, '') || 'unknown',
-          exportName,
-          tags: {},
-        }
-      })
-    )
+            return {
+              props,
+              slots,
+              events,
+              displayName:
+                componentPath
+                  .split('/')
+                  .pop()
+                  ?.replace(/\.(ts|js|vue)/, '') || 'unknown',
+              exportName,
+              tags: {},
+            }
+          })
+        )
+      } catch (e) {
+        console.error('error parsing', componentPath)
+        reject(e)
+      }
+    })
   },
 })
