7344aade2b20e1cdc6b8e99a0418124896b01559
elevatebart <bart@cypress.io>
2023-03-21 13:07:12 -0500

    fix docgen with new typescript

--- docs/docgen/docgen.config.vue.cjs
+++ docs/docgen/docgen.config.vue.cjs
@@ -4,7 +4,7 @@ const path = require('path')
 
 const { createComponentMetaChecker } = require('vue-component-meta')
 
-const tsconfigPath = path.resolve(__dirname, '..', '..', './tsconfig.json')
+const tsconfigPath = path.resolve(__dirname, '..', '..', './tsconfig.vue.json')
 const checker = createComponentMetaChecker(tsconfigPath)
 
 module.exports = defineConfig({
@@ -17,59 +17,52 @@ module.exports = defineConfig({
     if (event === 'add') {
       checker.reload()
     }
-    return new Promise((resolve, reject) => {
-      try {
-        const exportNames = checker.getExportNames(componentPath)
-        resolve(
-          exportNames.map((exportName) => {
-            const meta = checker.getComponentMeta(componentPath, exportName)
+    const exportNames = checker.getExportNames(componentPath)
+    return Promise.resolve(
+      exportNames.map((exportName) => {
+        const meta = checker.getComponentMeta(componentPath, exportName)
 
-            const nonGlobalProps = meta.props.filter((prop) => !prop.global)
+        const nonGlobalProps = meta.props.filter((prop) => !prop.global)
 
-            // massage the output of meta to match the docgen format
-            const props = nonGlobalProps.length
-              ? nonGlobalProps.map((p) => ({
-                  ...p,
-                  type: {
-                    name: p.type,
-                  },
-                  tags: p.tags.reduce((acc, t) => {
-                    acc[t.name] = [{ title: t.name, content: t.text }]
-                    return acc
-                  }, {}),
-                }))
-              : undefined
+        // massage the output of meta to match the docgen format
+        const props = nonGlobalProps.length
+          ? nonGlobalProps.map((p) => ({
+              ...p,
+              type: {
+                name: p.type,
+              },
+              tags: p.tags.reduce((acc, t) => {
+                acc[t.name] = [{ title: t.name, content: t.text }]
+                return acc
+              }, {}),
+            }))
+          : undefined
 
-            const slots = meta.slots.length
-              ? meta.slots.map((s) => ({
-                  name: s.name,
-                }))
-              : undefined
+        const slots = meta.slots.length
+          ? meta.slots.map((s) => ({
+              name: s.name,
+            }))
+          : undefined
 
-            const events = meta.events.length
-              ? meta.events.map((s) => ({
-                  name: s.name,
-                }))
-              : undefined
+        const events = meta.events.length
+          ? meta.events.map((s) => ({
+              name: s.name,
+            }))
+          : undefined
 
-            return {
-              props,
-              slots,
-              events,
-              displayName:
-                componentPath
-                  .split('/')
-                  .pop()
-                  ?.replace(/\.(ts|js|vue)/, '') || 'unknown',
-              exportName,
-              tags: {},
-            }
-          })
-        )
-      } catch (e) {
-        console.error('error parsing', componentPath)
-        reject(e)
-      }
-    })
+        return {
+          props,
+          slots,
+          events,
+          displayName:
+            componentPath
+              .split('/')
+              .pop()
+              ?.replace(/\.(ts|js|vue)/, '') || 'unknown',
+          exportName,
+          tags: {},
+        }
+      })
+    )
   },
 })
