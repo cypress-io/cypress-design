ef3a61413f87215fd05fa293a55a35847d6a44e9
elevatebart <bart@cypress.io>
2023-04-11 17:45:03 -0500

    fix build

--- docs/src/BrandColors.vue
+++ docs/src/BrandColors.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import { colors as designColors } from '@cypress-design/css/dist/colors'
-import { filter, flatten, pick, startCase, startsWith, values } from 'lodash'
+import _ from 'lodash'
 import flat from 'flat'
 import chroma from 'chroma-js'
+
+const { filter, flatten, pick, startCase, startsWith, values } = _
 
 function contrastingTextColor(color: string) {
   const ratioToBlack = chroma.contrast(color, 'black')
