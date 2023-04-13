97ac41ab6969ae2c30badc77e44a6abeda017911
elevatebart <bart@cypress.io>
2023-04-13 09:37:14 -0500

    cy: remove import of tailwind styles

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'virtual:windi.css'
-import '../../style.css'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
