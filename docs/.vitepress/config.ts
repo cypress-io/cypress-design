3bd24e6ba6fc56f384fb45f76c6e16eb7907b42f
elevatebart <bart@cypress.io>
2023-04-18 14:56:16 -0500

    fix icons

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
       },
       config: {
         safelist: safeColors(),
-        // important: false,
+        important: false,
         extract: {
           extractors: [
             {
