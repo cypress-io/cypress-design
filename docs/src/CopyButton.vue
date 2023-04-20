08451a745a313cc22654f42e309b0f8f4b5a7c32
elevatebart <bart@cypress.io>
2023-04-20 17:51:14 -0500

    add compi buttons

---
+++ docs/src/CopyButton.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+defineProps<{
+  text: string
+}>()
+
+const copiedSuccess = ref(false)
+function copyCode(text: string) {
+  const el = document.createElement('textarea')
+  el.value = text
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
+  copiedSuccess.value = true
+  setTimeout(() => {
+    copiedSuccess.value = false
+  }, 1000)
+}
+</script>
+
+<template>
+  <button
+    v-if="!copiedSuccess"
+    class="invisible group-hover:visible text-indigo-500 text-[12px]"
+    @click="(e) => copyCode(text)"
+  >
+    copy
+  </button>
+  <span v-else class="text-green-500 text-[12px]">copied</span>
+</template>
