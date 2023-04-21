67d8f7013a45b494341725177ebb38991fe10042
elevatebart <bart@cypress.io>
2023-04-21 11:19:18 -0500

    better copy button

---
+++ docs/src/IconButton.vue
@@ -0,0 +1,149 @@
+<script lang="ts" setup>
+import { computed, nextTick, ref } from 'vue'
+import Icon from '@cypress-design/vue-icon'
+import _ from 'lodash'
+import CopyButton from './CopyButton.vue'
+
+const { upperFirst, camelCase } = _
+
+const props = defineProps<{
+  iconName: string
+  groupName: string
+  meta: Record<string, string[]>
+  focused: boolean
+}>()
+
+const localFocused = ref(false)
+
+const focused = computed(() => props.focused || localFocused.value)
+
+const IconAny = Icon as any
+
+const placeholderStyle = ref({
+  width: '1px',
+  height: '1px',
+  top: '0',
+  left: '0',
+})
+
+const buttonStyle = ref({
+  top: '0',
+  height: '0',
+})
+
+function focus() {
+  placeholderStyle.value = {
+    top: `${$button.value?.offsetTop}px`,
+    height: `${$button.value?.offsetHeight}px`,
+    width: `${$button.value?.offsetWidth}px`,
+    left: `${$button.value?.offsetLeft}px`,
+  }
+  buttonStyle.value = {
+    top: `${($button.value?.offsetTop ?? 0) + 4}px`,
+    height: `${($button.value?.offsetHeight ?? 0) - 8}px`,
+  }
+  nextTick(() => {
+    localFocused.value = true
+  })
+}
+
+const $button = ref<HTMLDivElement>()
+</script>
+
+<template>
+  <div
+    v-if="localFocused"
+    class="absolute w-full h-full top-0 left-0 z-10 bg-gray-500/70"
+    @click="localFocused = false"
+  />
+  <div
+    v-if="localFocused"
+    class="bg-indigo-500 rounded"
+    :style="placeholderStyle"
+  />
+  <div
+    ref="$button"
+    class="gap-x-[16px] flex flex-wrap items-end mx-auto overflow-hidden bg-indigo-50 dark:bg-gray-800"
+    :class="{
+      'mx-[16px] px-[8px] pb-[4px] rounded md:flex-nowrap justify-end md:justify-start':
+        focused,
+      'rounded py-[8px] justify-center': !focused,
+      'absolute left-[64px] right-[64px] z-20 w-auto': localFocused,
+      'w-[calc(100%-32px)] lg:w-[700px]': !localFocused && focused,
+    }"
+    :style="localFocused ? buttonStyle : undefined"
+  >
+    <p
+      class="text-[16px] flex-shrink-0 overflow-hidden whitespace-nowrap overflow-hidden py-[4px]"
+      :class="{
+        'w-full text-left md:text-right md:w-auto md:min-w-[300px]': focused,
+        hidden: !focused,
+      }"
+    >
+      <span class="flex items-center justify-end mb-[8px] gap-x-[8px] group"
+        ><CopyButton :text="iconName" /><code>{{ iconName }}</code></span
+      >
+      <span class="flex items-center justify-end gap-x-[8px] group"
+        ><CopyButton
+          :text="`&lt;Icon${upperFirst(camelCase(iconName))}/&gt;`"
+        /><code>&lt;Icon{{ upperFirst(camelCase(iconName)) }} /&gt;</code></span
+      >
+    </p>
+    <div
+      v-for="size in meta.availableSizes"
+      :key="size"
+      class="flex gap-[8px] items-end group"
+    >
+      <div
+        class="py-[4px] min-w-[32px] flex flex-col items-center gap-x-[16px] gap-y-[4px] justify-end"
+        :class="{
+          'pl-[4px] border-l border-gray-300': focused,
+          'px-[8px] md:px-[4px] md:w-[164px]': !focused,
+        }"
+      >
+        <IconAny :name="iconName" :size="size" />
+        <button
+          v-if="!focused"
+          class="text-indigo-500 inline text-[12px] invisible group-hover:visible"
+          @click="focus()"
+        >
+          show me code
+        </button>
+        <p class="text-gray-500 text-[12px]">
+          <span v-if="!focused">{{
+            iconName.slice(groupName.length + 1)
+          }}</span>
+          {{ size }}
+        </p>
+      </div>
+      <div
+        v-if="focused"
+        :key="`${iconName}_${size}`"
+        class="text-center text-teal-500"
+      >
+        <div v-if="meta.hasStrokeColor && meta.hasStrokeColor.includes(size)">
+          s
+        </div>
+        <div v-if="meta.hasFillColor && meta.hasFillColor.includes(size)">
+          f
+        </div>
+        <div
+          v-if="
+            meta.hasSecondaryStrokeColor &&
+            meta.hasSecondaryStrokeColor.includes(size)
+          "
+        >
+          s+
+        </div>
+        <div
+          v-if="
+            meta.hasSecondaryFillColor &&
+            meta.hasSecondaryFillColor.includes(size)
+          "
+        >
+          f+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- docs/src/IconsGrid.vue
+++ docs/src/IconsGrid.vue
@@ -1,13 +1,7 @@
 <script lang="ts" setup>
-import { ref, computed, nextTick } from 'vue'
+import { ref, computed } from 'vue'
 import { iconsMetadata } from '@cypress-design/icon-registry'
-import Icon from '@cypress-design/vue-icon'
-import _ from 'lodash'
-import CopyButton from './CopyButton.vue'
-
-const IconAny = Icon as any
-
-const { upperFirst, camelCase } = _
+import IconButton from './IconButton.vue'
 
 const search = ref('')
 const $searchInput = ref<HTMLInputElement>()
@@ -50,101 +44,18 @@ const groupedIconsMetadata = computed(() =>
       <div
         class="flex"
         :class="{
-          'max-w-full flex-wrap px-[24px] gap-x-[16px]': !search.length,
+          'max-w-full flex-wrap px-[24px] gap-[16px]': !search.length,
           'flex-col items-start': search.length,
         }"
       >
-        <div
+        <IconButton
           v-for="(meta, iconName) of icons"
           :key="iconName"
-          class="mt-[16px] gap-x-[16px] flex flex-wrap items-end mx-auto overflow-hidden bg-indigo-50 dark:bg-gray-800"
-          :class="{
-            'w-[calc(100%-32px)] mx-[16px] px-[8px] pb-[4px] rounded lg:w-[600px] md:flex-nowrap justify-end md:justify-start':
-              search.length,
-            'rounded py-[8px] justify-center': !search.length,
-          }"
-        >
-          <p
-            class="text-[16px] flex-shrink-0 overflow-hidden whitespace-nowrap overflow-hidden py-[4px]"
-            :class="{
-              'w-full text-left md:text-right md:w-[250px]': search.length,
-              hidden: !search.length,
-            }"
-          >
-            <span
-              class="flex items-center justify-end mb-[8px] gap-x-[8px] group"
-              ><CopyButton :text="iconName" />{{ iconName }}</span
-            >
-            <span class="flex items-center justify-end gap-x-[8px] group"
-              ><CopyButton
-                :text="`&lt;Icon${upperFirst(camelCase(iconName))}/&gt;`"
-              />&lt;Icon{{ upperFirst(camelCase(iconName)) }} /&gt;</span
-            >
-          </p>
-          <div
-            v-for="size in meta.availableSizes"
-            :key="size"
-            class="flex gap-[8px] items-end group"
-          >
-            <div
-              class="py-[4px] min-w-[32px] flex flex-col items-center gap-x-[16px] gap-y-[4px] justify-end"
-              :class="{
-                'pl-[4px] border-l border-gray-300': search.length,
-                'px-[8px] md:px-[4px] md:w-[164px]': !search.length,
-              }"
-            >
-              <IconAny :name="iconName" :size="size" />
-              <button
-                v-if="!search.length"
-                class="text-indigo-500 inline text-[12px] invisible group-hover:visible"
-                @click="
-                  () => {
-                    $searchInput?.focus()
-                    search = iconName
-                  }
-                "
-              >
-                focus
-              </button>
-              <p class="text-gray-500 text-[12px]">
-                <span v-if="!search.length">{{
-                  iconName.slice(groupName.length + 1)
-                }}</span>
-                {{ size }}
-              </p>
-            </div>
-            <div
-              v-if="search.length"
-              :key="`${iconName}_${size}`"
-              class="text-center text-teal-500"
-            >
-              <div
-                v-if="meta.hasStrokeColor && meta.hasStrokeColor.includes(size)"
-              >
-                s
-              </div>
-              <div v-if="meta.hasFillColor && meta.hasFillColor.includes(size)">
-                f
-              </div>
-              <div
-                v-if="
-                  meta.hasSecondaryStrokeColor &&
-                  meta.hasSecondaryStrokeColor.includes(size)
-                "
-              >
-                s+
-              </div>
-              <div
-                v-if="
-                  meta.hasSecondaryFillColor &&
-                  meta.hasSecondaryFillColor.includes(size)
-                "
-              >
-                f+
-              </div>
-            </div>
-          </div>
-        </div>
+          :iconName="iconName"
+          :meta="meta"
+          :groupName="groupName"
+          :focused="!!search.length"
+        />
       </div>
     </div>
   </div>
