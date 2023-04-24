81cd4c00ae396833eba304765b120ef7174a5ce4
Barthélémy Ledoux <bart@cypress.io>
2023-04-24 12:38:32 -0700

    fix: better tabs (#191)
    * remove loading transition for tabs
    
    * use unset instead of initial
    
    * allow href for tabs
    
    * add changeset

---
+++ .changeset/new-tabs-feat.md
@@ -0,0 +1,8 @@
+---
+'@cypress-design/react-tabs': minor
+'@cypress-design/vue-tabs': minor
+---
+
+- Make the static render show active tabs
+- On load, avoid the "boing" effect
+- Allow tabs to be navigable for A11y, UX and SEO
--- components/Tabs/constants.ts
+++ components/Tabs/constants.ts
@@ -23,6 +23,11 @@ export interface Tab {
    */
   iconBefore?: any
   tag?: string
+  /**
+   * allows for tabs to be links as well
+   * NOTE: the link will be prevented from navigating. It is only there for accessibility, semantics, SEO and right click -> open in new tab
+   */
+  href?: string
 }
 
 export const classesMap = {
@@ -32,6 +37,7 @@ export const classesMap = {
     button:
       'flex items-center px-[12px] h-[24px] leading-[20px] text-[14px] rounded font-medium whitespace-nowrap',
     active: 'text-black relative z-20',
+    activeStatic: '!text-white',
     inActive: 'hover:bg-gray-50',
     activeMarker:
       'absolute bottom-[4px] top-[4px] rounded duration-300 ease-in-out',
@@ -39,6 +45,8 @@ export const classesMap = {
       'bg-indigo-500 filter hue-rotate-180 brightness-200 saturate-150 z-10',
     activeMarkerBlender:
       'z-30 bg-white mix-blend-difference pointer-events-none',
+    activeMarkerStatic:
+      'bg-indigo-500 absolute -z-10 rounded right-0 left-0 bottom-0 top-0',
     tag: '',
   },
   'underline-small': {
@@ -47,12 +55,15 @@ export const classesMap = {
     button:
       'flex items-center px-[12px] h-[24px] leading-[20px] text-[14px] rounded font-medium whitespace-nowrap relative',
     active: 'text-gray-900 dark:text-gray-400 z-20',
+    activeStatic: 'relative',
     inActive:
       'before:transition-color before:duration-300 before:absolute hover:before:bg-gray-200 before:bottom-[-6.5px] before:h-[4px] before:left-0 before:right-0 before:rounded-full',
     activeMarker:
       'absolute bottom-[-2.5px] h-[4px] rounded-full z-10 duration-300 ease-in-out',
     activeMarkerColor: 'bg-indigo-500',
     activeMarkerBlender: 'hidden',
+    activeMarkerStatic:
+      'absolute bg-indigo-500 rounded-full right-0 left-0 bottom-[-6.5px] h-[4px]',
     tag: 'ml-[8px] px-[4px] bg-indigo-300/20 rounded text-gray-500 text-[12px] leading-[16px]',
   },
   'underline-large': {
@@ -61,12 +72,15 @@ export const classesMap = {
     button:
       'flex items-center px-[12px] h-[32px] leading-[24px] text-[16px] rounded font-medium whitespace-nowrap relative',
     active: 'text-gray-900 dark:text-gray-400 z-20',
+    activeStatic: 'relative',
     inActive:
       'before:transition-color before:duration-300 before:absolute hover:before:bg-gray-200 before:bottom-[-6.5px] before:h-[4px] before:left-0 before:right-0 before:rounded-full',
     activeMarker:
       'absolute bottom-[-2.5px] h-[4px] rounded-full z-10 duration-300 ease-in-out',
     activeMarkerColor: 'bg-indigo-500',
     activeMarkerBlender: 'hidden',
+    activeMarkerStatic:
+      'absolute bg-indigo-500 rounded-full right-0 left-0 bottom-[-6.5px] h-[4px]',
     tag: '',
   },
 } as const
