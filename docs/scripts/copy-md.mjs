587b213ae41d2861fe53e7c2a223b8e3e1ae7ff7
elevatebart <bart@cypress.io>
2023-04-27 21:10:34 -0500

    simplify component docs

--- docs/scripts/copy-md.mjs
+++ docs/scripts/copy-md.mjs
@@ -8,24 +8,14 @@ import * as url from 'url'
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 ;(async () => {
-  console.log(resolve(__dirname, '../../'))
-  const nonFwComponentReadmes = (
-    await globby('./components/*/*.md', {
-      cwd: resolve(__dirname, '../../'),
-    })
-  ).reduce((acc, path) => {
-    acc[resolve(__dirname, '../../', path)] = path.replace(
-      /\/ReadMe\.md$/i,
-      '.md'
-    )
-    console.log(path)
-    return acc
-  }, {})
   await Promise.all(
-    Object.entries(nonFwComponentReadmes).map(async ([source, dest]) => {
-      console.log({ source, dest })
-      // read the source file
-      const fileContents = await fs.readFile(source, 'utf8')
+    (
+      await globby('./components/*/*.md', {
+        cwd: resolve(__dirname, '../../'),
+      })
+    ).map(async (path) => {
+      const dest = path.replace(/\/ReadMe\.md$/i, '.md')
+      const fileContents = `# ${dest.split('/').pop()?.slice(0, -3)}\n`
 
       // create the destination directory
       const destDir = dirname(resolve(__dirname, '..', dest))
