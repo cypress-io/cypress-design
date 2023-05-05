d6489696cc53097462b712d8e2432c78e08e7ea4
elevatebart <bart@cypress.io>
2023-05-05 11:29:08 -0500

    remove icon case

--- components/icon/vue/Icon.ts
+++
@@ -1,9 +0,0 @@
-import { compileIcon } from '@cypress-design/icon-registry'
-import type { IconProps } from '@cypress-design/icon-registry'
-import { h } from 'vue'
-import type { SVGAttributes } from 'vue'
-import { compileVueIconProperties } from './compileProperties'
-
-export default (props: IconProps & Omit<SVGAttributes, 'name'>) => {
-  return h('svg', compileVueIconProperties(compileIcon(props)))
-}
--- components/icon/vue/index.ts
+++
@@ -1,7 +0,0 @@
-import type { iconsMetadata } from '@cypress-design/icon-registry'
-export type { WindiColor } from '@cypress-design/icon-registry'
-export { default } from './Icon'
-export { compileVueIconProperties } from './compileProperties'
-export * from './_TreeShakableIcons'
-
-export type IconId = keyof typeof iconsMetadata
