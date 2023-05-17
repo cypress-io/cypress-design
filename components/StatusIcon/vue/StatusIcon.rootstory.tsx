f754626f7acdcd08f6ed5958bd4cd23ed47d668d
elevatebart <bart@cypress.io>
2023-05-17 14:54:39 -0500

    fix most ts errors

--- components/StatusIcon/vue/StatusIcon.rootstory.tsx
+++ components/StatusIcon/vue/StatusIcon.rootstory.tsx
@@ -5,22 +5,22 @@ import { statuses } from '../constants'
 const sizes = ['4', '8', '12', '16', '24']
 
 export default () => (
-  <table className="w-full">
+  <table class="w-full">
     <thead>
       <tr key="h0">
-        <th className="text-left">Status</th>
-        <th className="text-left">Variant</th>
-        <th className="text-left" colSpan={sizes.length}>
+        <th class="text-left">Status</th>
+        <th class="text-left">Variant</th>
+        <th class="text-left" col-span={sizes.length}>
           Size
         </th>
-        <th className="text-left">Used for</th>
+        <th class="text-left">Used for</th>
       </tr>
       <tr key="h1">
         <th />
         <th />
         {sizes.map((size) => {
           return (
-            <th key={`heading-${size}`} className="text-left">
+            <th key={`heading-${size}`} class="text-left">
               {size}
             </th>
           )
@@ -37,7 +37,7 @@ export default () => (
             {statusInfo.variants.map((variant, i) => {
               return (
                 <tr
-                  className={clsx(i === 0 && 'border-t')}
+                  class={clsx(i === 0 && 'border-t')}
                   key={`${status}-${variant}`}
                 >
                   {i === 0 ? (
@@ -45,7 +45,7 @@ export default () => (
                       {statusInfo.link ? (
                         <a
                           href={statusInfo.link}
-                          className="text-indigo-500 underline"
+                          class="text-indigo-500 underline"
                           target="_blank"
                         >
                           {status}
@@ -61,7 +61,7 @@ export default () => (
                   <td>{variant}</td>
                   {sizes.map((size) => {
                     return (
-                      <td key={`${status}-${size}-${variant}`} className="py-2">
+                      <td key={`${status}-${size}-${variant}`} class="py-2">
                         <StatusIcon
                           status={status}
                           size={size}
@@ -70,7 +70,7 @@ export default () => (
                       </td>
                     )
                   })}
-                  <td className="py-2 align-top">
+                  <td class="py-2 align-top">
                     {i === 0 ? statusInfo.use : ''}
                   </td>
                 </tr>
