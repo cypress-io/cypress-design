74a930bb7fec6866b71af8f36a9a4335831fdaed
elevatebart <bart@cypress.io>
2023-05-17 18:23:11 -0500

    better docgen config

--- components/Icon/vue/Icon.ts
+++ components/Icon/vue/Icon.ts
@@ -1,10 +1,14 @@
 import { compileIcon } from '@cypress-design/icon-registry'
-import type { IconProps } from '@cypress-design/icon-registry'
+import type { OpenIconProps, IconProps } from '@cypress-design/icon-registry'
 import { h } from 'vue'
 import type { SVGAttributes } from 'vue'
 import { compileVueIconProperties } from './compileProperties'
 
-export default (props: IconProps & Omit<SVGAttributes, 'name'>) => {
+export default (
+  // the OpenIconProps helps volar extract the documentation from the props
+  // since the IconProps are more restrictive, it will not change the use behavior
+  props: OpenIconProps & IconProps & Omit<SVGAttributes, 'name'>
+) => {
   const { class: className, ...otherProps } = props
   const properties = compileVueIconProperties(compileIcon(otherProps))
   return h('svg', {
