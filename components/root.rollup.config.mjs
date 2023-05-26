3867f323df5867ecd5a650688b4d2122691864a6
elevatebart <bart@cypress.io>
2023-05-26 11:18:08 -0500

    make sure dependencies are set right

--- components/root.rollup.config.mjs
+++ components/root.rollup.config.mjs
@@ -1,7 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import { Plugin as TailwindKeepRollupPlugin } from '@cypress-design/rollup-plugin-tailwind-keep'
+import rollupPluginPackage from '@cypress-design/rollup-plugin-tailwind-keep'
+const { Plugin: TailwindKeepRollupPlugin } = rollupPluginPackage
 
 export default ({ input, plugins = [] }) => ({
   input,
