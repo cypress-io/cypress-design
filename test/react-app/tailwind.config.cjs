317cec378da2f20c6debb4c692bcd11a28073961
elevatebart <bart@cypress.io>
2023-05-26 11:21:36 -0500

    fix all the things

---
+++ test/react-app/tailwind.config.cjs
@@ -0,0 +1,22 @@
+const cypressCSS = require('@cypress-design/css')
+const filePaths = [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}',
+  '../../components/*/react/dist/*.@(js|css)',
+]
+
+// intended way
+// module.exports = cypressCSS.TailwindConfig(filePaths)
+
+// tailwind way\
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [cypressCSS.TailwindConfig()],
+  content: {
+    files: filePaths,
+    extract: ['vue', 'js', 'tsx'].reduce((acc, ext) => {
+      acc[ext] = cypressCSS.TailwindIconExtractor
+      return acc
+    }, {}),
+  },
+}
