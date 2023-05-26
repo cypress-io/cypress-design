926311a1f3ef608053c3bff0553c53b553d62753
elevatebart <bart@cypress.io>
2023-05-26 11:29:17 -0500

    remove call to css plugin

--- test/react-app/vite.config.ts
+++ test/react-app/vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { CyCSSVitePlugin } from '@cypress-design/css'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    CyCSSVitePlugin({
-      scan: {
-        include: [
-          'src/**/*.@(tsx|ts)',
-          '../../components/*/react/dist/*.@(js|css)',
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 })
