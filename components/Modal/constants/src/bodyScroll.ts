f644e9604ec45f524ed98ab720c58a96b2ad251d
elevatebart <bart@cypress.io>
2023-06-05 11:20:12 -0500

    remove the body scroll

--- components/Modal/constants/src/bodyScroll.ts
+++ components/Modal/constants/src/bodyScroll.ts
@@ -1,19 +1,23 @@
+const ATTR_DATA_MODAL_SCROLL_TOP = 'data-modal-scroll-top'
+const CY_MODAL_CLASS = 'cy-modal-overflow-hidden'
+
 export function disableBodyScroll() {
-  document.body.style.top = `-${window.scrollY}px`
+  document.body.setAttribute(ATTR_DATA_MODAL_SCROLL_TOP, `${window.scrollY}`)
   if (document.documentElement.clientWidth > window.innerWidth) {
     document.documentElement.classList.add('cy-modal-overflow-scroll-x')
   }
   if (document.documentElement.clientHeight > window.innerHeight) {
     document.documentElement.classList.add('cy-modal-overflow-scroll-y')
   }
-  document.body.classList.add('cy-modal-overflow-hidden')
+  document.body.classList.add(CY_MODAL_CLASS)
 }
 
 export function freeBodyScroll() {
-  const scrollY = parseInt(document.body.style.top || '0')
-  document.body.style.top = ''
-  document.body.classList.remove('cy-modal-overflow-hidden')
+  const scrollY = parseInt(
+    document.body.getAttribute(ATTR_DATA_MODAL_SCROLL_TOP) || '0'
+  )
+  document.body.classList.remove(CY_MODAL_CLASS)
   document.documentElement.classList.remove('cy-modal-overflow-scroll-x')
   document.documentElement.classList.remove('cy-modal-overflow-scroll-y')
-  window.scrollTo({ top: -scrollY })
+  window.scrollTo({ top: scrollY })
 }
