5010ac8c17ac266989b73352cad503e486c259f6
elevatebart <bart@cypress.io>
2023-06-13 13:12:41 +0200

    fix and add a test

--- components/Checkbox/vue/Checkbox.rootstory.tsx
+++
@@ -1,9 +0,0 @@
-import Checkbox from './Checkbox.vue'
-
-export default (options: {
-  color?: 'jade' | 'indigo' | 'red'
-  checked?: boolean
-}) => {
-  const { checked = true, color } = options
-  return <Checkbox checked={checked} label="Checked" color={color} />
-}
