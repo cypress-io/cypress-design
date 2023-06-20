bdf687fc258ad48f94454e57f27c3b6fc486379f
elevatebart <bart@cypress.io>
2023-06-20 17:42:03 +0200

    add comment

--- icon-registry/build.sh
+++ icon-registry/build.sh
@@ -1,3 +1,7 @@
+# for some reason rollup hates to work with files that "just" got created
+# we create this script to run the build to make sure that build can happen 
+# without hickups
+
 node node_modules/.bin/svg-to-ts-constants
 node ./build-icons.mjs
 node ../node_modules/.bin/rollup -c ./rollup.config.mjs
