674deae02e9b95d2c38a33fdd41c898821f8eb4c
Chris B. France <7366+christopherfrance@users.noreply.github.com>
2023-07-17 15:43:25 -0700

    WIP Prototyping with Figma Tailwind export

--- components/NavigationSide/react/NavigationSide.tsx
+++ components/NavigationSide/react/NavigationSide.tsx
@@ -2,12 +2,54 @@ import * as React from 'react'
 import clsx from 'clsx'
 // import { SharedSettings } from '@cypress-design/constants-navigationside'
 import { ProjectDropdown } from './ProjectDropdown'
-
+import { NavLink } from './_NavLink'
+import { IconTechnologyCodeEditor } from '@cypress-design/react-icon'
 export interface NavigationSideProps {
   id: string
   label?: string
   className?: string
 }
+
+const items = [
+  {
+    active: true,
+    href: '/specs',
+    text: 'specs',
+    collapsible: true,
+  },
+  {
+    href: '/debug',
+    text: 'debug',
+    collapsible: true,
+  },
+  {
+    href: '/runs',
+    text: 'runs',
+    collapsible: true,
+  },
+  {
+    href: '/reviews',
+    text: 'reviews',
+    collapsible: true,
+  },
+  {
+    href: '/branches',
+    text: 'branches',
+    collapsible: true,
+  },
+  {
+    href: '/insights',
+    text: 'insights',
+    collapsible: true,
+  },
+  {
+    href: '/settings',
+    text: 'settings',
+    collapsible: true,
+  },
+]
+
+const collapsible = true
 
 export const NavigationSide: React.FC<
   NavigationSideProps & React.HTMLProps<HTMLDivElement>
@@ -15,6 +57,11 @@ export const NavigationSide: React.FC<
   return (
     <div {...rest} id={id} className={clsx('bg-gray-900', className)}>
       <ProjectDropdown />
+      <ul {...rest} className={clsx('list-none p-0', rest?.className)}>
+        {items.map((item, index) => (
+          <NavLink key={index} item={item} collapsible={collapsible} />
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ components/NavigationSide/react/_NavLink.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react'
+import clsx from 'clsx'
+import { NavItemLink } from '@cypress-design/constants-docmenu'
+
+export interface NavLinkProps {
+  item: string
+  collapsible: boolean
+  depth?: number
+  onActive?: (top: number) => void
+}
+
+export const NavLink: React.FC<NavLinkProps> = ({
+  item,
+  collapsible,
+  depth = -1,
+  onActive,
+}) => {
+  const activeLIRef = React.useRef<HTMLLIElement>(null)
+
+  // on mount, if the item is active,
+  // send the top position to the parent
+  React.useEffect(() => {
+    if (item.active) {
+      onActive?.(activeLIRef?.current?.offsetTop || 0)
+    }
+  }, [])
+
+  return (
+    <li
+      ref={activeLIRef}
+      className={clsx('list-none p-0', {
+        'pl-[16px]': depth >= 0,
+      })}
+    >
+      <a
+        className={clsx('group relative inline-block pl-[24px]', {
+          'text-indigo-500': item.active,
+          'py-[8px] text-[16px] leading-[24px]': depth < 0,
+          'py-[12px] leading-[20px] text-[14px]': depth >= 0,
+        })}
+        href={item.href}
+      >
+        {depth >= 0 ? (
+          <div
+            className={clsx(
+              'absolute w-[4px] z-10 top-[10%] h-[80%] rounded-full hidden',
+              {
+                'group-hover:block bg-gray-300': !item.active && collapsible,
+              }
+            )}
+            style={{
+              left: `-${18.5 + depth * 8}px`,
+            }}
+          />
+        ) : null}
+        {item.text}
+      </a>
+    </li>
+  )
+}
