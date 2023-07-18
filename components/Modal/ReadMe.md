9ec1a81cbe0a136bb8bd74b5af968c41615cefbc
Ryan Wilke <ryanwilke@gmail.com>
2023-07-18 19:18:58 +0200

    fix: improved Modal Styles (#281)
    * Fixed the styles in React
    
    * Fixed the modal border to be an outline
    
    * Fixed the responsive styles of the modals
    
    * fix: add variant children to tailwind
    
    * add changesets
    
    ---------
    
    Co-authored-by: elevatebart <bart@cypress.io>

--- components/Modal/ReadMe.md
+++ components/Modal/ReadMe.md
@@ -7,16 +7,6 @@ const visible = ref(false)
 
 # Modal
 
-<DemoWrapper>
-	<Modal v-model:show="visible" title="modal">
-    Lorem ipsum dolor sit amet consectetur
-    adipisicing elit. Id perspiciatis hic ad minima ex recusandae autem
-    incidunt, perferendis, illo voluptatum repudiandae iste voluptate
-    reiciendis quam officiis voluptas laboriosam eligendi explicabo!
-  </Modal>
-  <Button @click="visible = true">Open Modal</Button>
-</DemoWrapper>
-
-Describe your component here.
+As a general rule, modals are used when a quick action is required by the user. They should be avoided when a dedicated page is a preferrable due to the limitations of screen size, preserving browser history, etc.
 
 [figma::Modal](https://www.figma.com/file/1WJ3GVQyMV5e7xVxPg3yID/Design-System%2C-v1.x---%40latest?type=design&node-id=11381-16819&t=eTHkHYIn7QpvUPjI-4)
--- components/Modal/constants/src/index.ts
+++ components/Modal/constants/src/index.ts
@@ -1,21 +1,21 @@
 export const ClassBackDrop =
-  'fixed left-0 top-0 z-[1000010] h-screen w-screen bg-gray-900/[.92] backdrop-blur'
+  'fixed left-0 top-0 z-[1000010] h-screen w-screen bg-gray-900/[.80] backdrop-blur-sm'
 export const ClassModalContainer =
   'pointer-events-none fixed left-0 top-0 z-[1000020] h-full w-screen overflow-hidden flex items-center justify-center'
 export const ClassModal =
-  'pointer-events-auto relative flex flex-col rounded-lg border-4 border-gray-700 bg-white'
+  'pointer-events-auto relative flex flex-col rounded outline outline-4 outline-gray-500/[.40] bg-white'
 export const ClassModalStandardDimensions =
-  'max-h-[80vh] min-w-[200px] sm:min-w-[390px] sm:max-w-[70vw] shrink-0'
+  'max-h-[80vh] min-w-[200px] sm:min-w-[390px] max-w-[calc(100vw-32px)] sm:max-w-[70vw] shrink-0'
 export const ClassModalFullscreenDimensions =
   'h-[calc(100vh-32px)] sm:w-[calc(100vw-32px)]'
 export const ClassTitleBox =
-  'flex shrink-0 items-center justify-between rounded-t-md border-b border-gray-100 p-[16px] px-[24px]'
+  'flex shrink-0 items-center justify-between rounded-t-md border-b border-gray-100 p-[16px] px-16px sm:px-[24px]'
 export const ClassTitle = 'text-[18px] font-medium leading-[28px] text-gray-900'
 export const ClassHelpLinkDash = 'mx-[8px] h-[1px] w-[32px] bg-gray-100'
 export const ClassHelpLink =
   'flex items-center text-[16px] font-normal leading-[24px] text-indigo-500'
 export const ClassCloseButton =
   'group rounded-full border-transparent p-[4px] outline-none'
-export const ClassContent = 'overflow-y-auto p-[16px]'
+export const ClassContent = 'overflow-y-auto p-[16px] sm:px-[24px]'
 
 export * from './bodyScroll'
