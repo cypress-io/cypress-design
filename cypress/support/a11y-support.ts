fcd33f8de37b9846481f29cc9734e2ecf9fbe79a
elevatebart <bart@cypress.io>
2023-07-24 15:37:23 +0200

    only change what is necessary

--- cypress/support/a11y-support.ts
+++ cypress/support/a11y-support.ts
@@ -2,6 +2,26 @@ import 'cypress-axe'
 
 beforeEach(() => {
   cy.injectAxe()
+  cy.configureAxe({
+    rules: [
+      {
+        id: 'html-has-lang',
+        enabled: false,
+      },
+      {
+        id: 'landmark-one-main',
+        enabled: false,
+      },
+      {
+        id: 'page-has-heading-one',
+        enabled: false,
+      },
+      {
+        id: 'region',
+        enabled: false,
+      },
+    ],
+  })
 })
 
 afterEach(() => {
