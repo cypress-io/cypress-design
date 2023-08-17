a0194e0f045059a64bf1b0b378d4b6b8b12d02f2
elevatebart <bart@cypress.io>
2023-08-17 20:55:22 +0200

    update transition of card

---
+++ .changeset/css-card-transition.md
@@ -0,0 +1,5 @@
+---
+'@cypress-design/css': patch
+---
+
+update transition of card
--- css/src/shortcuts.ts
+++ css/src/shortcuts.ts
@@ -12,89 +12,108 @@ interface RecursiveKeyValuePair {
  * outline with that, do border-transparent for the non-hocus state.
  */
 
-export const tailwindPlugin = plugin(function ({
-  addComponents,
-  addUtilities,
-  addBase,
-  theme,
-}) {
-  function defaultRing(
-    color: 'indigo' | 'error' | 'jade'
-  ): RecursiveKeyValuePair {
-    return {
-      borderWidth: theme('borderWidth.DEFAULT'),
-      borderColor: theme(`borderColor.${color}.300`),
-      boxShadow: `0 0 0 2px ${theme(`colors.${color}.100`)}`,
-      transition: 'all 150ms ease-in-out',
-      outline: '2px solid transparent',
-      ['&:disabled']: {
-        boxShadow: 'none',
-        borderColor: 'transparent',
-      },
+export const tailwindPlugin = plugin(
+  function ({ addComponents, addUtilities, addBase, theme }) {
+    function defaultRing(
+      color: 'indigo' | 'error' | 'jade',
+    ): RecursiveKeyValuePair {
+      return {
+        borderWidth: theme('borderWidth.DEFAULT'),
+        borderColor: theme(`borderColor.${color}.300`),
+        boxShadow: `0 0 0 2px ${theme(`colors.${color}.100`)}`,
+        outline: '2px solid transparent',
+        animation: '.2s ease-in-out 0s 1 normal none running cardHover',
+        ['&:disabled']: {
+          boxShadow: 'none',
+          borderColor: 'transparent',
+        },
+      }
     }
-  }
 
-  function makeFocusDefaultObject(
-    variants: Array<'focus' | 'hover' | 'focus-within'>,
-    color: 'indigo' | 'error' | 'jade' = 'indigo'
-  ): RecursiveKeyValuePair {
-    return {
-      outline: 'none',
-      transition: 'all 150ms ease-in-out',
-      [variants.map((variant) => `&:${variant}`).join(',')]: defaultRing(color),
+    function makeFocusDefaultObject(
+      variants: Array<'focus' | 'hover' | 'focus-within'>,
+      color: 'indigo' | 'error' | 'jade' = 'indigo',
+    ): RecursiveKeyValuePair {
+      return {
+        outline: 'none',
+        [variants.map((variant) => `&:${variant}`).join(',')]:
+          defaultRing(color),
+      }
     }
-  }
 
-  addComponents({
-    '.card': {
-      background: 'white',
-      borderWidth: theme('borderWidth.DEFAULT'),
-      borderStyle: 'solid',
-      borderColor: theme('colors.gray.100'),
-      borderRadius: theme('borderRadius.DEFAULT'),
-      cursor: 'pointer',
-      display: 'block',
-      width: '100%',
-      ...makeFocusDefaultObject(['hover', 'focus']),
-    },
-    '.default-ring': defaultRing('indigo'),
-    '.hocus-within-default': makeFocusDefaultObject(['hover', 'focus-within']),
-    '.hocus-default': makeFocusDefaultObject(['hover', 'focus']),
-    '.focus-within-default': makeFocusDefaultObject(['focus-within']),
-    '.focus-default': makeFocusDefaultObject(['focus']),
-    '.hocus-link-default': {
-      '&:focus': {
-        outline: 'transparent',
-        textDecoration: 'underline',
+    addUtilities({
+      '@keyframes cardHover': {
+        '0%': { borderColor: theme('colors.gray.100') },
+        '100%': { borderColor: theme('colors.indigo.300') },
       },
-      '&:hover': {
-        textDecoration: 'underline',
+    })
+
+    addComponents({
+      '.card': {
+        background: 'white',
+        borderWidth: theme('borderWidth.DEFAULT'),
+        borderStyle: 'solid',
+        borderColor: theme('colors.gray.100'),
+        borderRadius: theme('borderRadius.DEFAULT'),
+        cursor: 'pointer',
+        display: 'block',
+        width: '100%',
+        ...makeFocusDefaultObject(['hover', 'focus']),
+      },
+      '.default-ring': defaultRing('indigo'),
+      '.hocus-within-default': makeFocusDefaultObject([
+        'hover',
+        'focus-within',
+      ]),
+      '.hocus-default': makeFocusDefaultObject(['hover', 'focus']),
+      '.focus-within-default': makeFocusDefaultObject(['focus-within']),
+      '.focus-default': makeFocusDefaultObject(['focus']),
+      '.hocus-link-default': {
+        '&:focus': {
+          outline: 'transparent',
+          textDecoration: 'underline',
+        },
+        '&:hover': {
+          textDecoration: 'underline',
+        },
+      },
+      '.hocus-error': makeFocusDefaultObject(['hover', 'focus'], 'error'),
+      '.hocus-secondary': makeFocusDefaultObject(['hover', 'focus'], 'jade'),
+    })
+
+    addUtilities({
+      // utilities for modal
+      '.cy-modal-overflow-hidden': {
+        position: 'relative',
+        overflow: 'hidden',
+      },
+      '.cy-modal-overflow-scroll-x': {
+        overflowX: 'scroll',
+      },
+      '.cy-modal-overflow-scroll-y': {
+        overflowY: 'scroll',
+      },
+    })
+
+    addBase({
+      '#modal-target': {
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        zIndex: '1000000',
+      },
+    })
+  },
+  {
+    theme: {
+      extend: {
+        keyframes: {
+          border: {
+            '0%': { borderColor: '#F0F1FF' },
+            '100%': { borderColor: '#a5b4fc' },
+          },
+        },
       },
     },
-    '.hocus-error': makeFocusDefaultObject(['hover', 'focus'], 'error'),
-    '.hocus-secondary': makeFocusDefaultObject(['hover', 'focus'], 'jade'),
-  })
-
-  addUtilities({
-    // utilities for modal
-    '.cy-modal-overflow-hidden': {
-      position: 'relative',
-      overflow: 'hidden',
-    },
-    '.cy-modal-overflow-scroll-x': {
-      overflowX: 'scroll',
-    },
-    '.cy-modal-overflow-scroll-y': {
-      overflowY: 'scroll',
-    },
-  })
-
-  addBase({
-    '#modal-target': {
-      position: 'absolute',
-      top: '0',
-      left: '0',
-      zIndex: '1000000',
-    },
-  })
-})
+  },
+)
