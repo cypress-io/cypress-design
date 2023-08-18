814b5d6eac7518fcf9a23bef4f40904d7c498c8b
Chris B. France <7366+christopherfrance@users.noreply.github.com>
2023-08-17 18:01:19 -0700

    Refactor tests to use interface as args

--- components/NavigationLayout/react/NavigationLayoutReact.cy.tsx
+++ components/NavigationLayout/react/NavigationLayoutReact.cy.tsx
@@ -1,15 +1,31 @@
-/// <reference types="cypress" />
-
 import * as React from 'react'
 import { mount } from 'cypress/react18'
 import NavigationLayoutStory from './NavigationLayout.rootstory'
 import assertions from '../assertions'
+import { NavigationLayoutInterface } from '../constants/dist'
 
-describe('NavigationLayout', () => {
-  function mountStory(
-    options: Parameters<typeof NavigationLayoutStory>[0] = {},
-  ) {
-    mount(<NavigationLayoutStory {...options} />)
-  }
-  assertions(mountStory)
-})
+describe(
+  'NavigationLayout',
+  { viewportHeight: 800, viewportWidth: 300 },
+  () => {
+    function mountStory(args: NavigationLayoutInterface) {
+      const {
+        items,
+        currentProject,
+        currentOrganization,
+        projects,
+        navigation,
+      } = args
+      mount(
+        <NavigationLayoutStory
+          items={items}
+          currentProject={currentProject}
+          currentOrganization={currentOrganization}
+          projects={projects}
+          navigation={navigation}
+        />,
+      )
+    }
+    assertions(mountStory)
+  },
+)
