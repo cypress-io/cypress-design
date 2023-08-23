ec2d47f0d8b3d93bb730f3e23e8f368add56978c
Chris B. France <7366+christopherfrance@users.noreply.github.com>
2023-08-23 16:21:41 -0700

    WIP llm support

--- cypress/support/llm-support.ts
+++ cypress/support/llm-support.ts
@@ -1,9 +1,20 @@
-import { Configuration, OpenAIApi } from 'openai'
+import OpenAI from 'openai'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: 'process.env.OPENAI_API_KEY,',
 })
 
-export const openAi = new OpenAIApi(configuration)
+async function callOpenAI(text) {
+  const completion = openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+  })
+
+  return (await completion).choices[0].message
+}
+
+Cypress.on('uncaught:exception', async (err) => {
+  return await callOpenAI(err.toString())
+})
