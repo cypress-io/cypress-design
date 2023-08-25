1dc9a315962aebc2999b083d363092d8ecff3c03
elevatebart <bart@cypress.io>
2023-08-25 17:38:29 +0200

    move component closer to it's tests

--- components/StatusIcon/vue/StatusIconVue.cy.tsx
+++ components/StatusIcon/vue/StatusIconVue.cy.tsx
@@ -6,75 +6,6 @@ import { StatusIcon } from '@cypress-design/vue-statusicon'
 import StatusIconStory from './StatusIcon.rootstory'
 import { computed, defineComponent, ref } from 'vue'
 
-const VariableStatusIcon = defineComponent({
-  setup() {
-    const sizeSlider = ref(3)
-    const size = computed(() => sizes[sizeSlider.value])
-    const statusSlider = ref(0)
-    const status = computed(() => Object.keys(statuses)[statusSlider.value])
-    const variantSlider = ref(0)
-    const variant = computed(
-      () => ['simple', 'solid', 'outline'][variantSlider.value],
-    )
-
-    return () => {
-      return (
-        <div class="p-8">
-          <div class="flex items-center gap-4">
-            <label for="sizeRange" class="w-20">
-              Size
-            </label>
-            <input
-              id="sizeRange"
-              type="range"
-              min="0"
-              max="4"
-              step="1"
-              v-model={sizeSlider.value}
-            />
-            {size.value}
-          </div>
-          <div class="flex items-center gap-4">
-            <label for="statusRange" class="w-20">
-              Status
-            </label>
-            <input
-              id="statusRange"
-              type="range"
-              min="0"
-              max="12"
-              step="1"
-              v-model={statusSlider.value}
-            />
-            {status.value}
-          </div>
-          <div class="flex items-center gap-4">
-            <label for="variantRange" class="w-20">
-              Variant
-            </label>
-            <input
-              id="variantRange"
-              type="range"
-              min="0"
-              max="2"
-              step="1"
-              v-model={variantSlider.value}
-            />
-            {variant.value}
-          </div>
-          <div class="ml-20 h-20 w-[160px] flex items-center justify-center">
-            <StatusIcon
-              variant={variant.value}
-              size={size.value}
-              status={status.value}
-            />
-          </div>
-        </div>
-      )
-    }
-  },
-})
-
 describe('StatusIcon', () => {
   it('defaults to a variant that exists if the one provided does not exist', () => {
     mount(() => {
@@ -125,6 +56,75 @@ describe('StatusIcon', () => {
     cy.then(function () {
       expect(this.firstIcon).to.eq(this.secondIcon)
     })
+  })
+
+  const VariableStatusIcon = defineComponent({
+    setup() {
+      const sizeSlider = ref(3)
+      const size = computed(() => sizes[sizeSlider.value])
+      const statusSlider = ref(0)
+      const status = computed(() => Object.keys(statuses)[statusSlider.value])
+      const variantSlider = ref(0)
+      const variant = computed(
+        () => ['simple', 'solid', 'outline'][variantSlider.value],
+      )
+
+      return () => {
+        return (
+          <div class="p-8">
+            <div class="flex items-center gap-4">
+              <label for="sizeRange" class="w-20">
+                Size
+              </label>
+              <input
+                id="sizeRange"
+                type="range"
+                min="0"
+                max="4"
+                step="1"
+                v-model={sizeSlider.value}
+              />
+              {size.value}
+            </div>
+            <div class="flex items-center gap-4">
+              <label for="statusRange" class="w-20">
+                Status
+              </label>
+              <input
+                id="statusRange"
+                type="range"
+                min="0"
+                max="12"
+                step="1"
+                v-model={statusSlider.value}
+              />
+              {status.value}
+            </div>
+            <div class="flex items-center gap-4">
+              <label for="variantRange" class="w-20">
+                Variant
+              </label>
+              <input
+                id="variantRange"
+                type="range"
+                min="0"
+                max="2"
+                step="1"
+                v-model={variantSlider.value}
+              />
+              {variant.value}
+            </div>
+            <div class="ml-20 h-20 w-[160px] flex items-center justify-center">
+              <StatusIcon
+                variant={variant.value}
+                size={size.value}
+                status={status.value}
+              />
+            </div>
+          </div>
+        )
+      }
+    },
   })
 
   it('is reactive to size prop changes', () => {
