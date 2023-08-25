479d8975b13560c1df48085dde730ff41d07712f
elevatebart <bart@cypress.io>
2023-08-25 16:45:42 +0200

    remove duplicated assets

--- .changeset/failing-statusicon.md
+++ .changeset/failing-statusicon.md
@@ -1,9 +1,6 @@
 ---
 '@cypress-design/constants-statusicon': minor
 '@cypress-design/vue-statusicon': minor
-'@cypress-design/react-icon': minor
-'@cypress-design/vue-icon': minor
-'@cypress-design/icon-registry': minor
 ---
 
 add Failing icon to StatusIcon
--- components/StatusIcon/constants/src/simple-imports.ts
+++ components/StatusIcon/constants/src/simple-imports.ts
@@ -45,9 +45,6 @@ import {
   cyStatusCancelledSolidX24,
   cyStatusErroredSolidX16,
   cyStatusErroredSolidX24,
-  cyStatusFailingOutlineX4,
-  cyStatusFailingOutlineX8,
-  cyStatusFailingOutlineX12,
   cyStatusFailingOutlineX16,
   cyStatusFailingOutlineX24,
 } from '@cypress-design/icon-registry'
@@ -61,9 +58,9 @@ export const statuses: Record<string, IconSet> = {
     size24Icon: cyStatusRunningOutlineX24,
   },
   failing: {
-    size4Icon: cyStatusFailingOutlineX4,
-    size8Icon: cyStatusFailingOutlineX8,
-    size12Icon: cyStatusFailingOutlineX12,
+    size4Icon: cyStatusRunningSimpleX4,
+    size8Icon: cyStatusRunningSimpleX8,
+    size12Icon: cyStatusRunningOutlineX12,
     size16Icon: cyStatusFailingOutlineX16,
     size24Icon: cyStatusFailingOutlineX24,
   },
