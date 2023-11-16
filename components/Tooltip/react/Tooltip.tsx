4fcb9f6510557436cb49abbd9542a18d49cb6306
Barthélémy Ledoux <bart@cypress.io>
2023-11-16 17:56:58 +0100

    fix: accessible-tooltips (#344)
    * fix: add role to react tooltip and add a aria title
    
    * hide tooltip on press escape
    
    * add changeset
    
    * avoid using aria-label
    
    * Fix class to className in Tooltip component

--- components/Tooltip/react/Tooltip.tsx
+++ components/Tooltip/react/Tooltip.tsx
@@ -186,6 +186,7 @@ export const Tooltip: React.FC<
                   ...(interactive ? { padding: '16px' } : {}),
                 },
               })}
+              role="tooltip"
             >
               <div
                 className={clsx('rounded shadow-tooltip border', [
@@ -225,6 +226,7 @@ export const Tooltip: React.FC<
                     colors.background,
                   )}
                 >
+                  <span className="sr-only">Tooltip: </span>
                   {popper}
                 </div>
               </div>
