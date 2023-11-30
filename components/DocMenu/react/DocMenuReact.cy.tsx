1a294a4c4ac0e40856fe92fdcc0b31579cccf1ab
elevatebart <bart@cypress.io>
2023-11-30 15:59:35 +0100

    add a few small tests for react

--- components/DocMenu/react/DocMenuReact.cy.tsx
+++ components/DocMenu/react/DocMenuReact.cy.tsx
@@ -3,10 +3,36 @@
 import * as React from 'react'
 import { mount } from 'cypress/react18'
 import DocMenuStory from './DocMenu.rootstory'
+import DocMenu from './index'
 import assertions from '../assertions'
 import { NavGroup, NavItemLink } from '../constants'
 
 describe('<DocMenu/>', () => {
+  it('works with 1 level of items', () => {
+    mount(<DocMenu items={[{ href: '/foo', label: 'Foo' }]} />)
+  })
+
+  it('works with 2 level of items', () => {
+    mount(
+      <DocMenu
+        items={[{ label: 'Bar', items: [{ href: '/foo', label: 'Foo' }] }]}
+      />,
+    )
+  })
+
+  it('works with 3 level of items', () => {
+    mount(
+      <DocMenu
+        items={[
+          {
+            label: 'Baz',
+            items: [{ label: 'Bar', items: [{ href: '/foo', label: 'Foo' }] }],
+          },
+        ]}
+      />,
+    )
+  })
+
   function mountStory(items: (NavItemLink | NavGroup)[] = []) {
     mount(<DocMenuStory items={items} />)
   }
