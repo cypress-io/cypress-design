5459ddec4ab6cb62281c15db6cbbea9178d86ffd
elevatebart <bart@cypress.io>
2023-12-01 16:41:51 +0100

    add linkComponent prop on vue

--- components/DocMenu/vue/DocMenu.vue
+++ components/DocMenu/vue/DocMenu.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import type { DefineComponent } from 'vue'
 import { NavGroup, NavItemLink } from '@cypress-design/constants-docmenu'
 import DocGroup from './_DocGroup.vue'
 import DocLink from './_DocLink.vue'
@@ -7,10 +8,12 @@ withDefaults(
   defineProps<{
     items: (NavItemLink | NavGroup)[]
     collapsible?: boolean
+    linkComponent?: DefineComponent | 'a'
   }>(),
   {
+    linkComponent: 'a',
     collapsible: true,
-  }
+  },
 )
 </script>
 
@@ -18,9 +21,19 @@ withDefaults(
   <ul class="overflow-y-hidden list-none p-0">
     <template v-for="item in items">
       <li v-if="'items' in item" class="relative list-none p-0">
-        <DocGroup :group="item" :depth="0" :collapsible="collapsible" />
+        <DocGroup
+          :group="item"
+          :depth="0"
+          :collapsible="collapsible"
+          :link-component="linkComponent"
+        />
       </li>
-      <DocLink v-else :item="item" :collapsible="collapsible" />
+      <DocLink
+        v-else
+        :item="item"
+        :collapsible="collapsible"
+        :link-component="linkComponent"
+      />
     </template>
   </ul>
 </template>
--- components/DocMenu/vue/DocMenuVue.cy.tsx
+++ components/DocMenu/vue/DocMenuVue.cy.tsx
@@ -1,12 +1,50 @@
 /// <reference types="cypress" />
+import { type DefineComponent, defineComponent } from 'vue'
 import { mount } from 'cypress/vue'
 import assertions from '../assertions'
 import type { NavGroup, NavItemLink } from '../constants'
-import DocMenuStory from './DocMenu.rootstory'
+import DocMenu from './DocMenu.vue'
+
+const CustomLinkVue: DefineComponent<{ href: StringConstructor }> =
+  defineComponent({
+    props: {
+      href: String,
+    },
+    setup(props, { slots }) {
+      return () => (
+        <div>
+          {slots.default?.()} + href: {props.href}
+        </div>
+      )
+    },
+  })
 
 describe('<DocMenu/>', () => {
+  it('renders', () => {
+    mount(() => <DocMenu items={[{ href: '/foo', label: 'Foo' }]} />)
+    cy.get('ul').should('exist')
+  })
+
+  it('renders the custom link components', () => {
+    mount(() => (
+      <DocMenu
+        items={[
+          {
+            label: 'Baaaaaaz',
+            items: [{ label: 'Bar', items: [{ href: '/foo', label: 'Foo' }] }],
+          },
+        ]}
+        linkComponent={CustomLinkVue}
+      />
+    ))
+    cy.findByText('Bar').click()
+    cy.findByText('Foo + href: /foo', { selector: 'div' }).should('be.visible')
+  })
+
   function mountStory(items: (NavItemLink | NavGroup)[] = []) {
-    mount(() => <DocMenuStory items={items} />)
+    mount(() => {
+      return <DocMenu items={items} />
+    })
   }
   assertions(mountStory)
 })
