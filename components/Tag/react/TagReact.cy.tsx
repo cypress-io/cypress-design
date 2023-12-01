5c1bbc67b0dfeec24b87d8c79e733b72eab70c2a
elevatebart <bart@cypress.io>
2023-12-01 20:46:42 +0100

    fix Tag key stuff

--- components/Tag/react/TagReact.cy.tsx
+++ components/Tag/react/TagReact.cy.tsx
@@ -11,37 +11,35 @@ describe('Tag', () => {
     mount(
       <div className="flex flex-row flex-wrap items-start justify-center bg-gray-700 gap-6">
         {(Object.keys(SizeClasses) as Array<keyof typeof SizeClasses>).map(
-          (size) => {
-            return (
-              <div className="flex flex-col items-center gap-3 justify-center my-4 p-4 bg-white rounded-lg">
-                <h3 className="text-right">{size}</h3>
-                {(
-                  Object.keys(ColorClasses) as Array<keyof typeof ColorClasses>
-                ).map((color) => {
-                  return (
-                    <>
-                      <div
-                        key={color}
-                        className="flex items-center justify-center"
-                      >
-                        <Tag size={size} color={color}>
-                          {`{Tag}`}
-                        </Tag>
-                      </div>
-                      <div
-                        key={`${color}-2`}
-                        className="flex items-center justify-center"
-                      >
-                        <Tag size={size} color={color} dark>
-                          {`{Tag}`}
-                        </Tag>
-                      </div>
-                    </>
-                  )
-                })}
-              </div>
-            )
-          },
+          (size) => (
+            <div
+              key={`size-${size}`}
+              className="flex flex-col items-center gap-3 justify-center my-4 p-4 bg-white rounded-lg"
+            >
+              <h3 className="text-right">{size}</h3>
+              {(
+                Object.keys(ColorClasses) as Array<keyof typeof ColorClasses>
+              ).map((color) => {
+                return (
+                  <React.Fragment key={color}>
+                    <div className="flex items-center justify-center">
+                      <Tag size={size} color={color}>
+                        {`{Tag}`}
+                      </Tag>
+                    </div>
+                    <div
+                      key={`${color}-2`}
+                      className="flex items-center justify-center"
+                    >
+                      <Tag size={size} color={color} dark>
+                        {`{Tag}`}
+                      </Tag>
+                    </div>
+                  </React.Fragment>
+                )
+              })}
+            </div>
+          ),
         )}
       </div>,
     )
