a58be9322cfc64c64533ff8f8414a45ba90be751
elevatebart <bart@cypress.io>
2023-12-07 15:30:42 +0100

    fix(docgen): make buttons left-aligned (logical)

---
+++ .changeset/witty-jars-travel.md
@@ -0,0 +1,7 @@
+---
+'@cypress-design/constants-docmenu': patch
+'@cypress-design/react-docmenu': patch
+'@cypress-design/vue-docmenu': patch
+---
+
+fix(docgen): make buttons left-aligned (logical)
--- components/DocMenu/constants/src/index.ts
+++ components/DocMenu/constants/src/index.ts
@@ -13,7 +13,8 @@ export interface NavGroup extends NavItemBase {
 }
 
 export const classes = {
-  button: 'flex leading-[24px] py-[10px] items-center relative w-full',
+  button:
+    'flex leading-[24px] py-[10px] items-center relative w-full text-start',
   topButton: 'leading-[24px] text-[16px] pl-[24px] font-medium',
   leafButton: 'leading-[20px] text-[14px] pl-[40px]',
 } as const
