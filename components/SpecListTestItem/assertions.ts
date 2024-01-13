230d0af546020dbfa9f0fffb4bcb8964abca54d1
Ryan Wilke <ryan@ryanwilke.com>
2024-01-13 01:57:11 +0100

    finished moving most of the styles to tailwind classes

--- components/SpecListTestItem/assertions.ts
+++ components/SpecListTestItem/assertions.ts
@@ -3,7 +3,9 @@
 export default function assertions(mountStory: (options?: any) => void): void {
   it('renders', () => {
     mountStory()
-    cy.viewport(500, 400)
-    cy.viewport(1280, 400)
+    cy.viewport(640, 640)
+    cy.viewport(768, 768)
+    cy.viewport(1024, 1024)
+    cy.viewport(1280, 1280)
   })
 }
--- components/SpecListTestItem/vue/SpecListTestItem.vue
+++ components/SpecListTestItem/vue/SpecListTestItem.vue
@@ -3,18 +3,25 @@
     class="test-item-container relative my-[-1px] hover:z-10 hover:outline outline-[3px] outline-gray-50 transition-all rounded-[1px] mix-blend-darken @container"
   >
     <div
-      class="test-item-row flex border border-gray-100 hover:border-gray-300 transition-all cursor-pointer justify-start items-center flex-nowrap px-[16px] h-[52px]"
       data-cy="test-item-row"
+      class="test-item-row flex border border-gray-100 hover:border-gray-300 transition-all cursor-pointer justify-start items-center flex-nowrap px-[16px] h-[52px]"
     >
-      <div class="test-item-list test-item-list-describes">
-        <div class="test-item-icon pr-[8px]" data-cy="test-item-icon">
-          <StatusIcon size="16" :status="status" variant="solid" />
+      <div
+        data-cy="test-item-list"
+        class="test-item-list flex justify-start items-center flex-nowrap overflow-hidden w-[100%] h-[100%]"
+      >
+        <div class="test-item-icon h-[16px] pr-[8px]" data-cy="test-item-icon">
+          <StatusIcon
+            size="16"
+            :status="status"
+            variant="solid"
+            class="block"
+          />
         </div>
         <template v-for="(name, index) in names" :key="index">
           <div
             data-cy="test-item-name"
-            :data-index="index"
-            class="test-item"
+            class="test-item overflow-hidden w-[max-content] min-w-[16px] max-w-[max-content]"
             :class="{
               'test-item-first grow shrink basis-[max-content]': index === 0,
               'test-item-middle shrink-100000 basis-auto':
@@ -23,9 +30,16 @@
                 index === names.length - 1,
             }"
           >
-            <span data-cy="test-item-text">&nbsp;{{ name }}</span>
+            <span
+              data-cy="test-item-text"
+              class="relative inline-block w-[100%] whitespace-nowrap overflow-hidden text-ellipsis min-w-[24px] left-[-4px]"
+              >&nbsp;{{ name }}</span
+            >
           </div>
-          <div class="test-item-chevron" v-if="index < name.length">
+          <div
+            class="test-item-chevron pr-[8px] pl-[6px] relative bottom-[-1px]"
+            v-if="index < names.length - 1"
+          >
             <IconChevronRightSmall stroke-color="gray-300" />
           </div>
         </template>
@@ -45,92 +59,63 @@ defineProps<{
 </script>
 
 <style lang="scss" scoped>
-.test-item {
-  overflow: hidden;
-  width: max-content;
-  max-width: max-content;
-  min-width: 16px;
+// .test-item {
+//   &-container {
+//     container: item / inline-size;
+//   }
 
-  &-container {
-    container: item / inline-size;
-  }
+//   &-row {
+//     @container item (max-width: 600px) {
+//       height: auto;
+//       padding: 12px 16px;
+//     }
+//   }
 
-  &-row {
-    @container item (max-width: 600px) {
-      height: auto;
-      padding: 12px 16px;
-    }
-  }
+//   &-icon {
+//     @container item (max-width: 600px) {
+//       position: absolute;
+//       left: 16px;
+//       top: 50%;
+//       margin-top: -8px;
+//     }
+//   }
 
-  &-icon {
-    @container item (max-width: 600px) {
-      position: absolute;
-      left: 16px;
-      top: 50%;
-      margin-top: -8px;
-    }
-  }
+//   &-middle {
+//     @container item (max-width: 600px) {
+//       flex-basis: 0;
+//       color: #888;
+//       font-size: 14px;
+//     }
+//   }
 
-  &-middle {
-    @container item (max-width: 600px) {
-      flex-basis: 0;
-      color: #888;
-      font-size: 14px;
-    }
-  }
+//   &-first {
+//     @container item (max-width: 600px) {
+//       flex-basis: 0;
+//       color: #888;
+//       font-size: 14px;
+//     }
+//   }
 
-  &-first {
-    @container item (max-width: 600px) {
-      flex-basis: 0;
-      color: #888;
-      font-size: 14px;
-    }
-  }
+//   &-last {
+//     @container item (max-width: 600px) {
+//       display: flex;
+//       flex-shrink: 0;
+//       flex-basis: 100%;
+//     }
+//   }
 
-  &-last {
-    @container item (max-width: 600px) {
-      display: flex;
-      flex-shrink: 0;
-      flex-basis: 100%;
-    }
-  }
+//   &-list {
+//     @container item (max-width: 600px) {
+//       flex-wrap: wrap;
+//       padding-left: 28px;
+//       flex-direction: row;
+//     }
+//   }
 
-  &-list {
-    list-style: none;
-    display: flex;
-    justify-content: left;
-    align-items: center;
-    flex-wrap: nowrap;
-    width: 100%;
-    overflow: hidden;
-
-    @container item (max-width: 600px) {
-      flex-wrap: wrap;
-      padding-left: 28px;
-      flex-direction: row;
-    }
-  }
-
-  &-chevron {
-    padding: 0 8px 0 6px;
-    color: #aaa;
-    position: relative;
-    bottom: -1px;
-
-    @container item (max-width: 600px) {
-      padding: 0 4px 0 2px;
-    }
-  }
-}
-
-span {
-  position: relative;
-  display: inline-block;
-  width: 100%;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  min-width: 24px;
-  left: -4px;
-}
+//   &-chevron {
+//     @container item (max-width: 600px) {
+//       padding: 0 4px 0 2px;
+//     }
+//   }
+// }
 </style>
