fc2791e4ce68b7fa103d3af8b73dab7d006d24fa
Armando Vasquez <mando@mcnuggies.dev>
2024-01-16 16:40:20 -0600

    stubbed test cases - logic tbd

--- components/Radio/vue/RadioVue.cy.tsx
+++ components/Radio/vue/RadioVue.cy.tsx
@@ -3,19 +3,31 @@ import { mount } from 'cypress/vue'
 import Radio from './Radio.vue'
 
 describe('<Radio/>', () => {
-  it('renders with label text', () => {
-    mount(() => <Radio label="Hello" id="radio" />)
+  it('renders with label text as a string', () => {
+    mount(() => <Radio label="Hello" id="some-radio" />)
+  })
+
+  it('renders with label text as a component', () => {
+    mount(() => <Radio label="Hello" id="some-radio" />)
   })
 
   it('renders without label text', () => {
-    mount(() => <Radio label="Hello" id="radio" />)
+    mount(() => <Radio label="Hello" id="some-radio" />)
   })
 
   it('changes when the label is clicked', () => {
-    mount(() => <Radio label="Hello" id="radio" />)
+    mount(() => <Radio label="Hello" id="some-radio" />)
   })
 
   it('changes when the radio is clicked', () => {
-    mount(() => <Radio label="Hello" id="radio" />)
+    mount(() => <Radio label="Hello" id="some-radio" />)
+  })
+
+  it('cannot be clicked when disabled', () => {
+    mount(() => <Radio label="Hello" id="some-radio" disabled />)
+  })
+
+  it('can be marked as checked, even if disabled', () => {
+    mount(() => <Radio label="Hello" id="some-radio" disabled checked />)
   })
 })
