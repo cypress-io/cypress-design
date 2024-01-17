3510c5ef3a1d385ecb9930265897ddb87755cad1
Ryan Wilke <ryan@ryanwilke.com>
2024-01-17 18:32:05 +0100

    added a failing test on purpose.

--- components/TestResult/assertions.ts
+++ components/TestResult/assertions.ts
@@ -1,12 +1,33 @@
 /// <reference types="cypress" />
 
 export default function assertions(mountStory: (options?: any) => void): void {
-  it('renders', () => {
-    mountStory()
-    cy.viewport(360, 480)
-    cy.viewport(640, 480)
-    cy.viewport(768, 480)
-    cy.viewport(1024, 480)
-    cy.viewport(1280, 480)
+  describe('viewport', () => {
+    before(() => {
+      mountStory()
+    })
+
+    afterEach(() => {
+      cy.percySnapshot()
+    })
+
+    it('xs', () => {
+      cy.viewport(360, 480)
+    })
+
+    it('sm', () => {
+      cy.viewport(640, 480)
+    })
+
+    it('md', () => {
+      cy.viewport(1024, 480)
+    })
+
+    it('lg', () => {
+      cy.viewport(1280, 480)
+    })
+
+    it('fails as expected', () => {
+      expect(true).to.equal(false)
+    })
   })
 }
