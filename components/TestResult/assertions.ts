ffed54284c120301adeb24487aa3fe5a39311760
Ryan Wilke <ryan@ryanwilke.com>
2024-01-17 19:20:03 +0100

    Update assertions.ts

--- components/TestResult/assertions.ts
+++ components/TestResult/assertions.ts
@@ -11,23 +11,23 @@ export default function assertions(mountStory: (options?: any) => void): void {
     })
 
     it('xs', () => {
-      cy.viewport(360, 480)
+      cy.viewport(360, 400)
     })
 
     it('sm', () => {
-      cy.viewport(640, 480)
+      cy.viewport(640, 400)
     })
 
     it('md', () => {
-      cy.viewport(1024, 480)
+      cy.viewport(1024, 400)
     })
 
     it('lg', () => {
-      cy.viewport(1280, 480)
+      cy.viewport(1280, 400)
     })
 
-    it('fails as expected', () => {
-      expect(true).to.equal(false)
-    })
+    // it('fails as expected', () => {
+    //   expect(true).to.equal(false)
+    // })
   })
 }
