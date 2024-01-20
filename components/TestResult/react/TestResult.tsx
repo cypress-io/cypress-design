f4920dfc00cb5dc7a091729e2f8678f56096f50b
Ryan Wilke <ryan@ryanwilke.com>
2024-01-20 01:40:52 +0100

    Add the React version of the component.

--- components/TestResult/react/TestResult.tsx
+++ components/TestResult/react/TestResult.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react'
-import clsx from 'clsx'
+import Button from '@cypress-design/react-button'
+import { CSS } from '@cypress-design/constants-testresult'
+import { StatusIcon } from '@cypress-design/react-statusicon'
+import {
+  IconChevronDownSmall,
+  IconChevronRightSmall,
+  IconStatusFlaky,
+  IconDocumentModifiedSquareDot,
+  IconDocumentAddedSquarePlus,
+} from '@cypress-design/react-icon'
 
 export interface TestResultProps {
   id: string
@@ -9,11 +18,105 @@ export interface TestResultProps {
 
 export const TestResult: React.FC<
   TestResultProps & React.HTMLProps<HTMLDivElement>
-> = ({ id, label, className, ...rest }) => {
+> = ({ status, flaky, modified, added, hasGroups, names }) => {
   return (
-    <div {...rest} id={id} className={clsx('bg-jade-100', className)}>
-      <label>{label}</label>
-      Render Function for TestResult
+    <div data-cy="cd-tr-container" className={CSS.container}>
+      <div data-cy="cd-tr-row" className={CSS.row}>
+        <div data-cy="cd-tr-list" className={CSS.list}>
+          <div data-cy="cd-tr-icon" className={CSS.icon}>
+            <StatusIcon
+              size="16"
+              variant="solid"
+              status={status}
+              className={CSS.status_icon}
+            />
+          </div>
+          <div
+            data-cy="cd-tr-name-container-column"
+            className={CSS.name.container.column}
+          >
+            {names.slice(0, -1).length > 0 && (
+              <div
+                data-cy="cd-tr-name-container-describes"
+                className={CSS.name.container.describes}
+              >
+                {names.slice(0, -1).map((name, index) => (
+                  <React.Fragment key={index}>
+                    <div
+                      data-cy="cd-tr-name-item"
+                      className={{
+                        [CSS.name.item.base]: true,
+                        [CSS.name.item.first]: names.length >= 2 && index === 0,
+                        [CSS.name.item.middle]:
+                          names.length >= 2 &&
+                          index > 0 &&
+                          index < names.length - 1,
+                      }}
+                    >
+                      <span
+                        data-cy="cd-tr-name-item-text"
+                        className={CSS.name.item.text.base}
+                      >
+                        {name}
+                      </span>
+                    </div>
+                    {index < names.length - 1 && (
+                      <div
+                        data-cy="cd-tr-chevron"
+                        className={CSS.chevron.container}
+                      >
+                        <IconChevronRightSmall
+                          strokeColor="gray-200"
+                          className="align-top relative bottom-[-1px]"
+                        />
+                      </div>
+                    )}
+                  </React.Fragment>
+                ))}
+              </div>
+            )}
+            <div
+              data-cy="cd-tr-name-container-it"
+              className={CSS.name.container.it}
+            >
+              <span
+                data-cy="cd-tr-name-item-text"
+                className={[
+                  CSS.name.item.text.base,
+                  CSS.name.item.text.it,
+                ].join(' ')}
+              >
+                {names.at(-1)}
+              </span>
+              {(flaky || modified || added) && (
+                <div
+                  data-cy="cd-tr-attributes"
+                  className={CSS.attribute.container}
+                >
+                  {flaky && <IconStatusFlaky data-cy="cd-tr-flaky" />}
+                  {modified && (
+                    <IconDocumentModifiedSquareDot data-cy="cd-tr-modified" />
+                  )}
+                  {added && (
+                    <IconDocumentAddedSquarePlus data-cy="cd-tr-added" />
+                  )}
+                </div>
+              )}
+            </div>
+          </div>
+          <div data-cy="cd-tr-actions" className={CSS.button.container}>
+            {/* Replace <slot></slot> with children if needed */}
+            <Button
+              variant="outline-light"
+              size="32"
+              className={CSS.button.chevron}
+            >
+              {!hasGroups && <IconChevronRightSmall strokeColor="gray-500" />}
+              {hasGroups && <IconChevronDownSmall strokeColor="gray-500" />}
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/TestResult/react/TestResultReact.cy.tsx
+++ components/TestResult/react/TestResultReact.cy.tsx
@@ -3,11 +3,42 @@
 import * as React from 'react'
 import { mount } from 'cypress/react18'
 import TestResult from './TestResult'
+import { TestResults } from '@cypress-design/constants-testresult'
+import Button from '@cypress-design/react-button'
+import { IconActionTestReplay } from '@cypress-design/react-icon'
 import assertions from '../assertions'
 
-describe('TestResult', () => {
-  function mountStory(options: Parameters<typeof TestResult>[0] = { id: '1' }) {
-    mount(<TestResult {...options} />)
+describe('<TestResult/>', () => {
+  function mountStory(options = {}) {
+    mount(
+      <div className="p-[16px]">
+        <div>
+          {TestResults.map((result) => (
+            <TestResult
+              key={result.id} // Assuming each result has a unique 'id' for key
+              status={result.status}
+              names={result.names}
+              flaky={result.flaky}
+              modified={result.modified}
+              added={result.added}
+              hasGroups={result.hasGroups}
+            >
+              <Button
+                variant="outline-light"
+                size="32"
+                className="!px-[8px] @lg/test-result:!px-[12px] h-[32px]"
+              >
+                <IconActionTestReplay />
+                <span className="hidden @lg/test-result:inline ml-[8px]">
+                  Test Replay
+                </span>
+              </Button>
+            </TestResult>
+          ))}
+        </div>
+      </div>,
+    )
   }
+
   assertions(mountStory)
 })
