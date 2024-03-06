a1fc783ac0be592383e7fd71515abb4e79cbd57a
Barthélémy Ledoux <bart@cypress.io>
2024-03-06 18:17:25 +0100

    feat: watch icons when running vitepress (#400)

--- icon-registry/rollup.config.mjs
+++ icon-registry/rollup.config.mjs
@@ -1,29 +1,33 @@
 import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
+import resolvePlugin from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import pkg from './package.json' assert { type: 'json' }
+import * as url from 'url'
+import { resolve } from 'path'
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 export default {
-  input: './src/index.ts',
+  input: resolve(__dirname, './src/index.ts'),
   output: [
     {
-      file: './dist/index.umd.js',
+      file: resolve(__dirname, './dist/index.umd.js'),
       format: 'umd',
       exports: 'auto',
       sourcemap: true,
       name: 'CypressIconRegistry',
     },
     {
-      file: './dist/index.es.mjs',
+      file: resolve(__dirname, './dist/index.es.mjs'),
       format: 'esm',
       sourcemap: true,
     },
   ],
   plugins: [
-    resolve(),
+    resolvePlugin(),
     commonjs(),
     typescript({
-      tsconfig: './tsconfig.json',
+      tsconfig: resolve(__dirname, './tsconfig.json'),
     }),
   ],
   external: Object.keys(pkg.dependencies),
