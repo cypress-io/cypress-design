6486abbdd16e40c1d97d88cd09e32b752aa23f42
Barthélémy Ledoux <bart@cypress.io>
2024-03-20 19:23:44 +0100

    finish the icons registry

--- icon-registry/build-icons.mjs
+++ icon-registry/build-icons.mjs
@@ -8,11 +8,15 @@
 import { promises as fs } from 'fs'
 import buildAnimatedIcons from './build-animated-icons.mjs'
 import getIcons from './generate-icons-content.mjs'
+import buildLogoIcons from './build-logo-icons.mjs'
 
 Promise.all([
   getIcons().then((icons) => fs.writeFile('./src/icons.ts', icons)),
   buildAnimatedIcons().then((animatedIcons) =>
     fs.writeFile('./src/animated-icons.ts', animatedIcons),
+  ),
+  buildLogoIcons().then((logoIcons) =>
+    fs.writeFile('./src/logos.ts', logoIcons),
   ),
 ]).then(() =>
   // eslint-disable-next-line no-console
--- icon-registry/src/index.ts
+++ icon-registry/src/index.ts
@@ -1,3 +1,4 @@
 export * from './icons'
+export * from './logos'
 export * from './animated-icons'
 export * from './compileIcon'
