aa8e5a2bac5bec5ffb7ed650c9cc34e1b4b818fa
Barthélémy Ledoux <bart@cypress.io>
2024-03-28 16:09:48 +0100

    remove double container plugin in tailwind

---
+++ .changeset/rich-lions-sit.md
@@ -0,0 +1,5 @@
+---
+"@cypress-design/css": patch
+---
+
+remove double containerplugin
--- css/src/tailwind.config.ts
+++ css/src/tailwind.config.ts
@@ -71,7 +71,6 @@ export default (fileGlobs: string[] = []) => {
           },
         })
       }),
-      containerPlugin,
     ],
   })
 }
