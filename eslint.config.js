233a1dbf61a8ce843eec8499560a474ffb449c28
Barthélémy Ledoux <bart@cypress.io>
2024-04-23 15:08:57 +0200

    start eslint config from scratch

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import pluginVueConfig from 'eslint-plugin-vue/configs/recommended.js'
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  pluginVueConfig,
+]
