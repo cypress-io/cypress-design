7046a543ed4251beffcc52e53ff12d61caa17e20
Barthélémy Ledoux <bart@cypress.io>
2024-04-23 15:57:57 +0200

    some updates but no beef yet

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,47 @@
-import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
-import pluginVueConfig from 'eslint-plugin-vue/configs/recommended.js'
+import pluginReact from 'eslint-plugin-react'
+import pluginVue from 'eslint-plugin-vue'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
 
 export default [
   {
+    name: 'global ignores',
+    ignores: ['**/dist/**', '**/cache/**'],
+  },
+  {
+    files: ['*.cjs'],
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+      sourceType: 'script',
     },
   },
-  pluginJs.configs.recommended,
+  {
+    ...pluginJs.configs.recommended,
+    files: ['*.mjs', '*.js'],
+  },
   ...tseslint.configs.recommended,
-  pluginReactConfig,
-  pluginVueConfig,
+  {
+    name: 'react-components',
+    files: ['components/*/react/*.{ts,tsx}'],
+    plugins: {
+      'typescript-eslint': tseslint.plugin,
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+    },
+    rules: {
+      // ...pluginReact.configs.recommended.rules,
+      // ...pluginReactHooks.configs.recommended.rules,
+    },
+  },
+  {
+    name: 'vue-components',
+    files: ['components/*/vue/*.{ts,tsx,vue}'],
+    plugins: {
+      'typescript-eslint': tseslint.plugin,
+      vue: pluginVue,
+    },
+    rules: {
+      ...pluginVue.configs.recommended.rules,
+    },
+  },
 ]
