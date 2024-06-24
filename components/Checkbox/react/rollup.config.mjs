cfaf6ac42b9726e88dcfe719a506b9c1156115a0
Barthélémy Ledoux <bart@cypress.io>
2024-06-24 12:31:29 +0200

    use external dependencies in components

--- components/Checkbox/react/rollup.config.mjs
+++ components/Checkbox/react/rollup.config.mjs
@@ -1,3 +1,7 @@
 import rootRollupConfig from '../../root.rollup.config.mjs'
+import pkg from './package.json' assert { type: 'json' }
 
-export default rootRollupConfig({ input: './index.ts' })
+export default rootRollupConfig({
+  input: './index.ts',
+  external: Object.keys(pkg.dependencies),
+})
