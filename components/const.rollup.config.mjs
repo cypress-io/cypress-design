03c913ea48d20aa6688537ee27deccc0199951d6
Barthélémy Ledoux <bart@cypress.io>
2024-06-27 15:07:43 +0200

    refactor: use rollup for constants

---
+++ components/const.rollup.config.mjs
@@ -0,0 +1,30 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
+export default ({ input = './src/index.ts', plugins = [], external = [] }) => ({
+  input,
+  output: [
+    {
+      file: './dist/index.umd.js',
+      format: 'cjs',
+      exports: 'auto',
+      sourcemap: true,
+    },
+    {
+      file: './dist/index.es.mjs',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declarationMap: true,
+    }),
+    ...plugins,
+  ],
+  external,
+})
