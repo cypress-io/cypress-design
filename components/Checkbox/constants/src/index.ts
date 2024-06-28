b5ea46ce397c58b9da240fd373202e3a3562fa93
Barthélémy Ledoux <bart@cypress.io>
2024-06-28 11:53:19 +0200

    add prefixes to checkbox

--- components/Checkbox/constants/src/index.ts
+++ components/Checkbox/constants/src/index.ts
@@ -1,13 +1,13 @@
-export const Classes = {
+export const CssClasses = {
   wrapper: 'relative flex items-center',
   hiddenInput: 'absolute inset-0 w-0 h-0 opacity-0',
   labelTag: 'flex items-center',
   visibleCheckbox:
     'border border-solid rounded h-[16px] w-[16px] flex flex-shrink-0 items-center text-white',
   trueLabel: 'block ml-[8px] text-[16px] leading-[24px] font-light select-none',
-}
+} as const
 
-export const CheckboxColors = {
+export const CssCheckboxColors = {
   indigo: 'border-indigo-500 bg-indigo-400',
   jade: 'border-jade-500 bg-jade-400',
   red: 'border-red-500 bg-red-400',
