86c8616cb511b816f0f11e6c89b269cf78a94647
Barthélémy Ledoux <bart@cypress.io>
2024-06-28 10:07:49 +0200

    set up generation system

---
+++ packages/component-tailwind-plugins/index.mjs
@@ -0,0 +1,7 @@
+import Accordion from '@cypress-design/constants-Accordion/tailwind
+
+const componentPlugins = [
+  Accordion
+]
+  
+export default componentPlugins
---
+++ scripts/generate-component-tailwind-plugins-package.mjs
@@ -0,0 +1,72 @@
+import { resolve } from 'path'
+import { promises as fs } from 'fs'
+import { globby } from 'globby'
+import * as url from 'url'
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
+console.log(__dirname)
+
+async function createPackages() {
+  const constantsPackages = await globby(
+    './components/*/constants/package.json',
+    {
+      cwd: resolve(__dirname, '../'),
+    },
+  )
+
+  const constantsPackagesWithTailwindPlugin = []
+
+  for (const pkgPath of constantsPackages) {
+    const {
+      default: { exports },
+    } = await import(resolve(__dirname, '../', pkgPath), {
+      assert: { type: 'json' },
+    })
+    if (exports?.['./tailwind']) {
+      // add current package to the list of tailwind plugins
+      const pathArray = pkgPath.split('/')
+      constantsPackagesWithTailwindPlugin.push(
+        pathArray[pathArray.indexOf('package.json') - 2],
+      )
+    }
+  }
+
+  console.log('found components: ', constantsPackagesWithTailwindPlugin)
+  return constantsPackagesWithTailwindPlugin
+}
+
+async function generate() {
+  const pkgs = await createPackages()
+  await fs.writeFile(
+    resolve(__dirname, '../packages/component-tailwind-plugins/index.mjs'),
+    `${pkgs.map((p) => `import ${p.replace(/-/g, '_')} from '@cypress-design/constants-${p}/tailwind`)}
+
+const componentPlugins = [
+  ${pkgs.map((p) => p.replace(/-/g, '_')).join(',\n\t')}
+]
+  
+export default componentPlugins`,
+    { encoding: 'utf-8' },
+  )
+
+  await fs.writeFile(
+    resolve(__dirname, '../packages/component-tailwind-plugins/package.json'),
+    `{
+  "name": "@cypress-design/component-tailwind-plugins",
+  "version": "0.0.0",
+  "private": "true",
+  "exports": {
+    "import": "./index.mjs",
+    "types": "./dist/index.d.ts"
+  },
+  "devDependencies": {
+    ${pkgs.map((p) => `"@cypress-design/constants-${p}": "*",`).join('\n\t\t')}
+    "tailwindcss": "^3.4.3"
+  }
+}`,
+    { encoding: 'utf-8' },
+  )
+}
+
+generate()
