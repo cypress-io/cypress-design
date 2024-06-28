076e7a2fbc8698ae4d882a186e3072327390fa0a
Barthélémy Ledoux <bart@cypress.io>
2024-06-28 10:28:53 +0200

    remove unneeded dependency

--- icon-registry/rollup.config.mjs
+++ icon-registry/rollup.config.mjs
@@ -1,7 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolvePlugin from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import pkg from './package.json' assert { type: 'json' }
 import * as url from 'url'
 import { resolve } from 'path'
 
